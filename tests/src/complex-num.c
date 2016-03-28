12fe1e2c7ed3af0ad359d152e9b3b8ae72594208
Aoto Tokizawa <aoto.tokizawa@gmail.com>
2016-03-28 19:14:53 -0400

    added src for tests

---
+++ tests/src/complex-num.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define COMPLEX_PART_FMT "%li"
+typedef long int complex_part_t;
+
+struct complex_t {
+  complex_part_t real;
+  complex_part_t imag;
+};
+
+struct complex_t cn_add(struct complex_t a, struct complex_t b);
+struct complex_t cn_sub(struct complex_t a, struct complex_t b);
+struct complex_t cn_mul(struct complex_t a, struct complex_t b);
+struct complex_t cn_div(struct complex_t a, struct complex_t b);
+
+int main(int argc, char **argv) {
+  if (argc != 6) {
+    printf("usage: complex-num w x [+-*/] y z\n");
+    return 0;
+  }
+
+  struct complex_t a, b;
+  a.real = atol(argv[1]);
+  a.imag = atol(argv[2]);
+  b.real = atol(argv[4]);
+  b.imag = atol(argv[5]);
+
+  struct complex_t c;
+  switch (argv[3][0]) {
+  case '+':
+    c = cn_add(a, b);
+    break;
+  case '-':
+    c = cn_sub(a, b);
+    break;
+  case '*':
+    c = cn_mul(a, b);
+    break;
+  case '/':
+    c = cn_div(a, b);
+    break;
+  default:
+    return 1;
+  };
+
+  printf(COMPLEX_PART_FMT " + " COMPLEX_PART_FMT "i\n", c.real, c.imag);
+  return 0;
+}
+
+struct complex_t cn_add(struct complex_t a, struct complex_t b) {
+  struct complex_t c;
+  c.real = a.real + b.real;
+  c.imag = a.imag + b.imag;
+  return c;
+}
+struct complex_t cn_sub(struct complex_t a, struct complex_t b) {
+  struct complex_t c;
+  c.real = a.real - b.real;
+  c.imag = a.imag - b.imag;
+  return c;
+}
+struct complex_t cn_mul(struct complex_t a, struct complex_t b) {
+  struct complex_t c;
+  c.real = a.real * b.real - a.imag * b.imag;
+  c.imag = a.imag * b.real + a.real * b.imag;
+  return c;
+}
+struct complex_t cn_div(struct complex_t a, struct complex_t b) {
+  struct complex_t c;
+  complex_part_t denom = b.real * b.real + b.imag * b.imag;
+  c.real = (a.real * b.real + a.imag * b.imag) / denom;
+  c.imag = (a.imag * b.real - a.real * b.imag) / denom;
+  return c;
+}
