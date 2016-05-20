18ff71426de65739cdfa415b3ebb8a617a136cf3
Aoto Tokizawa <aoto.tokizawa@gmail.com>
2016-05-20 17:51:19 -0400

    implemented thunking out

--- lib/arch/thunk_x86_64.c
+++ lib/arch/thunk_x86_64.c
@@ -12,12 +12,13 @@ void foo(int x) {
 static CPUX86State cpu_state;
 
 typedef void (*thunk_proc_ty)(void);
-static thunk_proc_ty __jove_thunk_in_to;
-
-static uint64_t regbuff[16];
+static thunk_proc_ty __jove_thunk_buff;
 
 void get_rand(void) __attribute__((naked));
 void __jove_thunk_in(void) __attribute__((naked));
+
+void __jove_thunk_out(void) __attribute__((naked));
+void __jove_thunk_out_prologue(void) __attribute__((naked));
 
 int __jove_impl_get_rand(void);
 
@@ -26,7 +27,7 @@ __attribute__((naked)) void get_rand(void) {
       "movq %%rax, %[out_rax]\n"
 
       : // OutputOperands
-      [out_rax] "=m" (regbuff[R_EAX])
+      [out_rax] "=m" (cpu_state.regs[R_EAX])
 
       : // InputOperands
 
@@ -38,7 +39,7 @@ __attribute__((naked)) void get_rand(void) {
       "jmp __jove_thunk_in\n"
 
       : // OutputOperands
-      [thunk_in_to] "=m" (__jove_thunk_in_to)
+      [thunk_in_to] "=m" (__jove_thunk_buff)
 
       : // InputOperands
       [__jove_impl] "a" (__jove_impl_get_rand)
@@ -65,10 +66,6 @@ void __jove_thunk_in() {
       "movq %%r14, %[out_r14]\n"
       "movq %%r15, %[out_r15]\n"
       "movq %[in_rax], %%rax\n"
-
-      //
-      // efficiently copy local copy of regs to CPU state
-      //
 
       "call *%[in_proc]\n"
 
@@ -126,7 +123,7 @@ void __jove_thunk_in() {
       [in_r14] "m" (cpu_state.regs[14]),
       [in_r15] "m" (cpu_state.regs[15]),
 
-      [in_proc] "m" (__jove_thunk_in_to)
+      [in_proc] "m" (__jove_thunk_buff)
 
       : // Clobbers
       "rax",
@@ -145,6 +142,131 @@ void __jove_thunk_in() {
       "r13",
       "r14",
       "r15");
+}
+
+void __jove_thunk_out() {
+  __asm__ volatile(
+      "xchgq %[in_rbp], %%rbp\n"
+      "xchgq %[in_rsp], %%rsp\n"
+
+      : // OutputOperands
+
+      : // InputOperands
+      [in_rbp] "m" (cpu_state.regs[R_EBP]),
+      [in_rsp] "m" (cpu_state.regs[R_ESP])
+
+      : // Clobbers
+      "rbp",
+      "rsp");
+
+  __asm__ volatile(
+      // change return address to __jove_thunk_out_prologue
+      "movq %[prologue], (%%rsp)\n"
+
+      : // OutputOperands
+
+      : // InputOperands
+      [prologue] "r" (__jove_thunk_out_prologue)
+
+      : // Clobbers
+      );
+
+  __asm__ volatile(
+      "movq %[in_rax], %%rax\n"
+      "movq %[in_rbx], %%rbx\n"
+      "movq %[in_rcx], %%rcx\n"
+      "movq %[in_rdx], %%rdx\n"
+      "movq %[in_rsi], %%rsi\n"
+      "movq %[in_rdi], %%rdi\n"
+      "movq %[in_r8],  %%r8\n"
+      "movq %[in_r9],  %%r9\n"
+      "movq %[in_r10], %%r10\n"
+      "movq %[in_r11], %%r11\n"
+      "movq %[in_r12], %%r12\n"
+      "movq %[in_r13], %%r13\n"
+      "movq %[in_r14], %%r14\n"
+      "movq %[in_r15], %%r15\n"
+
+      "jmp *%[dst]\n"
+
+      : // OutputOperands
+
+      : // InputOperands
+      [in_rax] "m" (cpu_state.regs[R_EAX]),
+      [in_rbx] "m" (cpu_state.regs[R_EBX]),
+      [in_rcx] "m" (cpu_state.regs[R_ECX]),
+      [in_rdx] "m" (cpu_state.regs[R_EDX]),
+      [in_rsi] "m" (cpu_state.regs[R_ESI]),
+      [in_rdi] "m" (cpu_state.regs[R_EDI]),
+      [in_r8]  "m" (cpu_state.regs[8]),
+      [in_r9]  "m" (cpu_state.regs[9]),
+      [in_r10] "m" (cpu_state.regs[10]),
+      [in_r11] "m" (cpu_state.regs[11]),
+      [in_r12] "m" (cpu_state.regs[12]),
+      [in_r13] "m" (cpu_state.regs[13]),
+      [in_r14] "m" (cpu_state.regs[14]),
+      [in_r15] "m" (cpu_state.regs[15]),
+
+      [dst] "*m" (__jove_thunk_buff)
+
+      : // Clobbers
+      "rax",
+      "rbx",
+      "rcx",
+      "rdx",
+      "rsi",
+      "rdi",
+      "r8",
+      "r9",
+      "r10",
+      "r11",
+      "r12",
+      "r13",
+      "r14",
+      "r15");
+}
+
+void __jove_thunk_out_prologue() {
+  __asm__ volatile(
+      "xchgq %%rbp, %[out_rbp]\n"
+      "xchgq %%rsp, %[out_rsp]\n"
+      "movq %%rbx, %[out_rbx]\n"
+      "movq %%rcx, %[out_rcx]\n"
+      "movq %%rdx, %[out_rdx]\n"
+      "movq %%rsi, %[out_rsi]\n"
+      "movq %%rdi, %[out_rdi]\n"
+      "movq %%r8,  %[out_r8]\n"
+      "movq %%r9,  %[out_r9]\n"
+      "movq %%r10, %[out_r10]\n"
+      "movq %%r11, %[out_r11]\n"
+      "movq %%r12, %[out_r12]\n"
+      "movq %%r13, %[out_r13]\n"
+      "movq %%r14, %[out_r14]\n"
+      "movq %%r15, %[out_r15]\n"
+      "ret\n"
+
+      : // OutputOperands
+      [out_rax] "=m" (cpu_state.regs[R_EAX]),
+      [out_rbx] "=m" (cpu_state.regs[R_EBX]),
+      [out_rcx] "=m" (cpu_state.regs[R_ECX]),
+      [out_rdx] "=m" (cpu_state.regs[R_EDX]),
+      [out_rsi] "=m" (cpu_state.regs[R_ESI]),
+      [out_rdi] "=m" (cpu_state.regs[R_EDI]),
+      [out_rbp] "=m" (cpu_state.regs[R_EBP]),
+      [out_rsp] "=m" (cpu_state.regs[R_ESP]),
+      [out_r8]  "=m" (cpu_state.regs[8]),
+      [out_r9]  "=m" (cpu_state.regs[9]),
+      [out_r10] "=m" (cpu_state.regs[10]),
+      [out_r11] "=m" (cpu_state.regs[11]),
+      [out_r12] "=m" (cpu_state.regs[12]),
+      [out_r13] "=m" (cpu_state.regs[13]),
+      [out_r14] "=m" (cpu_state.regs[14]),
+      [out_r15] "=m" (cpu_state.regs[15])
+
+      : // InputOperands
+
+      : // Clobbers
+      );
 }
 
 int __jove_impl_get_rand(void) {
