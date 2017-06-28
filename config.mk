d2d619ed360e9a51f26cda182557b47e73589e68
Aoto Tokizawa <aoto.tokizawa@gmail.com>
2017-06-28 13:20:06 -0400

    makefile: tell user when they haven't defined env vars

--- config.mk
+++ config.mk
@@ -2,10 +2,18 @@
 ROOT_DIR := $(shell cd $(dir $(word $(words $(MAKEFILE_LIST)),$(MAKEFILE_LIST)));pwd)
 
 #qemutcg_archs += arm
-#qemutcg_archs += x86_64
+qemutcg_archs += x86_64
 #qemutcg_archs += i386
 #qemutcg_archs += mipsel
 qemutcg_archs += aarch64
+
+ifndef QEMU_BUILD_DIR
+$(error QEMU_BUILD_DIR is not set)
+endif
+
+ifndef QEMU_SRC_DIR
+$(error QEMU_SRC_DIR is not set)
+endif
 
 qemu_dir       := /home/aeden/qemu-build
 llvm_dir       := /usr
