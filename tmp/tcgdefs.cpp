06f0bf6fcbf03e9005d6561058cb9a9c086c2384
Aoto Tokizawa <aoto.tokizawa@gmail.com>
2018-04-28 19:25:59 -0400

    WIP

--- tmp/tcgdefs.cpp
+++ tmp/tcgdefs.cpp
@@ -18,9 +18,33 @@ int main(int argc, char** argv) {
 
   jove::tiny_code_generator_t tcg;
 
-  for (int i = 0, n = tcg._tcg_ctx.nb_globals; i < n; i++) {
+  auto string_of_tcg_type = [](TCGType ty) -> const char * {
+#define __CHK_TY(NM)                                                           \
+  do {                                                                         \
+    if (ty == TCG_TYPE_##NM)                                                   \
+      return #NM;                                                              \
+  } while (0)
+
+    __CHK_TY(I32);
+    __CHK_TY(I64);
+    __CHK_TY(V64);
+    __CHK_TY(V128);
+    __CHK_TY(V256);
+
+#undef __CHK_TY
+
+    return "";
+  };
+
+  int N = tcg._tcg_ctx.nb_globals;
+  for (int i = 0; i < N; i++) {
     TCGTemp &ts = tcg._tcg_ctx.temps[i];
-    printf("%s\n", ts.name);
+
+    printf("%s %s", string_of_tcg_type(ts.type), ts.name);
+    if (ts.mem_base)
+      printf(" @ %s+%#lx", ts.mem_base->name,
+             static_cast<unsigned long>(ts.mem_offset));
+    printf("\n");
   }
 
   return 0;
