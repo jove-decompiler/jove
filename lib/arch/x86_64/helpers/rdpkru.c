11bdade05cbaf96b18fbbc7cc0a830dbadec0a51
Aoto Tokizawa <aoto.tokizawa@gmail.com>
2019-07-05 14:46:47 -0400

    helpers/{r,w}dpkru: these modifications allow us to run a recompiled app with LD_BIND_NOW=1. we'll revisit these helpers in the future if/when we encounter their use.

--- lib/arch/x86_64/helpers/rdpkru.c
+++ lib/arch/x86_64/helpers/rdpkru.c
@@ -453,44 +453,12 @@ typedef struct CPUX86State {
     TPRAccess tpr_access_type;
 } CPUX86State;
 
-void cpu_x86_cpuid(CPUX86State *env, uint32_t index, uint32_t count,
-                   uint32_t *eax, uint32_t *ebx,
-                   uint32_t *ecx, uint32_t *edx);
-
-#define SVM_EXIT_CPUID		0x072
-
-void QEMU_NORETURN raise_exception_err_ra(CPUX86State *env, int exception_index,
-                                          int error_code, uintptr_t retaddr);
-
-void cpu_svm_check_intercept_param(CPUX86State *env1, uint32_t type,
-                                   uint64_t param, uintptr_t retaddr);
-
-# define GETPC() \
-    ((uintptr_t)__builtin_extract_return_addr(__builtin_return_address(0)))
-
-void helper_cpuid(CPUX86State *env)
-{
-    uint32_t eax, ebx, ecx, edx;
-
-    cpu_svm_check_intercept_param(env, SVM_EXIT_CPUID, 0, GETPC());
-
-    cpu_x86_cpuid(env, (uint32_t)env->regs[R_EAX], (uint32_t)env->regs[R_ECX],
-                  &eax, &ebx, &ecx, &edx);
-    env->regs[R_EAX] = eax;
-    env->regs[R_EBX] = ebx;
-    env->regs[R_ECX] = ecx;
-    env->regs[R_EDX] = edx;
+static void myabort(void) {
+  __builtin_trap();
+  __builtin_unreachable();
 }
 
-uint64_t helper_rdpkru(CPUX86State *env, uint32_t ecx)
-{
-    if ((env->cr[4] & CR4_PKE_MASK) == 0) {
-        raise_exception_err_ra(env, EXCP06_ILLOP, 0, GETPC());
-    }
-    if (ecx != 0) {
-        raise_exception_err_ra(env, EXCP0D_GPF, 0, GETPC());
-    }
-
-    return env->pkru;
+uint64_t helper_rdpkru(CPUX86State *env, uint32_t ecx) {
+  myabort();
+  return env->pkru;
 }
-
