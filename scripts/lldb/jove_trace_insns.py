4cdbc60d9ca0ee363a7337abb0ba1c78ca20b650
Aoto Tokizawa <aoto.tokizawa@gmail.com>
2019-07-23 07:54:47 -0400

    scripts/lldb/jove_trace_insns.py: be more like jove_trace.py with respect to running to the application entry point, before stepping

--- scripts/lldb/jove_trace_insns.py
+++ scripts/lldb/jove_trace_insns.py
@@ -23,73 +23,126 @@ def jove_trace_insns(debugger, command, result, dict):
     except:
         return
     target = debugger.GetSelectedTarget()
-    if target:
-        process = target.GetProcess()
-        if process:
-            thread = process.GetSelectedThread()
-            if thread:
-                p = subprocess.Popen(["/usr/bin/llvm-symbolizer",
-                "-print-address",
-                "-inlining=0",
-                "-pretty-print",
-                "-print-source-context-lines=20"],
-                stdin=subprocess.PIPE,
-                stdout=subprocess.PIPE,
-                stderr=subprocess.STDOUT);
+    if not target:
+        print("no target")
+        return
 
-                flags = fcntl.fcntl(p.stdout, fcntl.F_GETFL) # get current p.stdout flags
-                fcntl.fcntl(p.stdout, fcntl.F_SETFL, flags | os.O_NONBLOCK)
+    launch_info = target.GetLaunchInfo()
+    argv = [launch_info.GetArgumentAtIndex(i) for i in list(range(launch_info.GetNumArguments()))]
+    #print(argv)
 
-                lastAddr = 0
+    err = lldb.SBError()
+    process = target.Launch(debugger.GetListener(), argv, None, None, None, None, None, 0, True, err)
+    if not err.Success():
+        print("Error during launch: " + str(err))
+        return
 
-                while True:
-                    error = lldb.SBError()
-                    thread.StepInstruction(False, error)
-                    if error.Fail():
-                        print(error)
-                        break
+    #
+    # set the breakpoint that will be after all of the DSOs have been loaded
+    #
+    modules = target.modules
 
-                    reason = thread.GetStopReason();
-                    if reason == lldb.eStopReasonSignal or reason == lldb.eStopReasonThreadExiting:
-                        debugger.HandleCommand("bt")
-                        break
+    # the two modules already in memory should be the dynamic linker and the
+    # application
+    modules = list(filter(lambda mod: str(mod.GetFileSpec()).find("[vdso]") == -1 and str(mod.GetFileSpec()).find("/usr/lib/ld-") == -1, modules))
 
-                    frame = thread.GetFrameAtIndex(0)
-                    line_entry = frame.GetLineEntry()
-                    line_path = str(line_entry.GetFileSpec())
+    if len(modules) != 1:
+        print("unable to find application module")
+        return
 
-                    suffix = ".fake"
-                    if not line_path.endswith(suffix):
-                        continue
+    app_mod = modules[0]
 
-                    # check for non-zero line number
-                    line_addr = line_entry.GetLine()
-                    if line_addr == 0:
-                        continue
+    # now get the entry
+    app_entry = app_mod.GetObjectFileEntryPointAddress()
+    if not app_entry.IsValid():
+        print("failed to get application module entry point")
+        return
 
-                    if line_addr == lastAddr:
-                        continue
+    # and set the breakpoint
+    entry_bp = target.BreakpointCreateBySBAddress(app_entry)
+    if not entry_bp.IsValid():
+        print("Can't set a breakpoint on the module entry point")
+        return
 
-                    lastAddr = line_addr
-                    line_path = line_path[:-len(suffix)]
+    # go
+    err = process.Continue()
+    if not err.Success():
+        print("failed to continue")
+        return
 
-                    print(frame)
+    #
+    # no longer need entry breakpoint
+    #
+    target.BreakpointDelete(entry_bp.GetID())
 
-                    #
-                    # exec llvm-symbolizer
-                    #
-                    p.stdin.write('%s 0x%x\n' % (line_path, line_addr))
-                    p.stdin.flush()
+    # get the thread
+    thread = process.GetSelectedThread()
+    if not thread:
+        print("no thread")
+        return
 
-                    while True:
-                        try:
-                            print(p.stdout.read(4096))
-                        except IOError as err:
-                            break
-                        except OSError as err:
-                            break
+    p = subprocess.Popen(["/usr/bin/llvm-symbolizer",
+    "-print-address",
+    "-inlining=0",
+    "-pretty-print",
+    "-print-source-context-lines=20"],
+    stdin=subprocess.PIPE,
+    stdout=subprocess.PIPE,
+    stderr=subprocess.STDOUT);
 
-                (stdoutdata, stderrdata) = p.communicate('\n')
+    flags = fcntl.fcntl(p.stdout, fcntl.F_GETFL) # get current p.stdout flags
+    fcntl.fcntl(p.stdout, fcntl.F_SETFL, flags | os.O_NONBLOCK)
+
+    lastAddr = 0
+
+    while True:
+        error = lldb.SBError()
+        thread.StepInstruction(False, error)
+        if error.Fail():
+            print(error)
+            break
+
+        reason = thread.GetStopReason();
+        if reason == lldb.eStopReasonSignal or reason == lldb.eStopReasonThreadExiting:
+            debugger.HandleCommand("bt")
+            break
+
+        frame = thread.GetFrameAtIndex(0)
+        line_entry = frame.GetLineEntry()
+        line_path = str(line_entry.GetFileSpec())
+
+        suffix = ".fake"
+        if not line_path.endswith(suffix):
+            continue
+
+        # check for non-zero line number
+        line_addr = line_entry.GetLine()
+        if line_addr == 0:
+            continue
+
+        if line_addr == lastAddr:
+            continue
+
+        lastAddr = line_addr
+        line_path = line_path[:-len(suffix)]
+
+        print(frame)
+
+        #
+        # exec llvm-symbolizer
+        #
+        p.stdin.write('%s 0x%x\n' % (line_path, line_addr))
+        p.stdin.flush()
+
+        while True:
+            try:
+                print(p.stdout.read(4096))
+            except IOError as err:
+                break
+            except OSError as err:
+                break
+
+    (stdoutdata, stderrdata) = p.communicate('\n')
 
 
 def create_jove_trace_insns_options():
