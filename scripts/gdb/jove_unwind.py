9be71ca16e337ec13e1b2ecd5db012721dd52471
Aoto Tokizawa <aoto.tokizawa@gmail.com>
2019-10-08 20:11:42 -0400

    scripts/gdb: jove-unwind

---
+++ scripts/gdb/jove_unwind.py
@@ -0,0 +1,42 @@
+# This implements the "jove_unwind" command, usually installed
+# in the debug session like
+#   source ~/jove/scripts/gdb/jove_unwind.py
+# it is used to produce meaningful backtraces of recompiled-code.
+
+import gdb
+import subprocess
+
+class JoveUnwindCommand(gdb.Command):
+    def __init__(self):
+        super(JoveUnwindCommand, self).__init__("jove_unwind", gdb.COMMAND_DATA)
+
+    def invoke(self, arg, from_tty):
+        print("Hello!")
+        inferiors = gdb.inferiors()
+        for inferior in inferiors:
+            for thread in inferior.threads():
+                # Change to our threads context
+                thread.switch()
+
+                # Take a human readable copy of the backtrace, we'll need this for display later.
+                #o = gdb.execute('bt', to_string=True)
+
+                gdb.newest_frame()
+                cur_frame = gdb.selected_frame()
+                while cur_frame is not None:
+                    suffix = ".fake"
+                    sal = cur_frame.find_sal()
+                    st = sal.symtab
+                    if st is not None and st.fullname().endswith(suffix):
+                        path = st.fullname()
+                        path = path[:-len(suffix)]
+
+                        print(path)
+
+                        completedProcess = subprocess.run(["/usr/bin/llvm-symbolizer", "-print-address", "-inlining=0", "-pretty-print", "-print-source-context-lines=10"], input=('%s 0x%x' % (path, sal.line)), capture_output=True, text=True)
+                        print(completedProcess.stdout)
+                        print(completedProcess.stderr)
+
+                    cur_frame = cur_frame.older()
+
+JoveUnwindCommand()
