d0d26d44d14b968ab36520cd2b9aaf31449eff3c
Aoto Tokizawa <aoto.tokizawa@gmail.com>
2019-11-11 12:00:17 -0500

    dyn: comment-out debug print statement

--- tools/jove-dyn.cpp
+++ tools/jove-dyn.cpp
@@ -186,7 +186,7 @@ int main(int argc, char **argv) {
       _argv = &arg_vec.a[0];
 
       for (int i = prog_args_idx + 1; i < argc; ++i) {
-        llvm::outs() << llvm::formatv("argv[{0}] = {1}\n", i, argv[i]);
+        //llvm::outs() << llvm::formatv("argv[{0}] = {1}\n", i, argv[i]);
 
         opts::Args.push_back(argv[i]);
       }
