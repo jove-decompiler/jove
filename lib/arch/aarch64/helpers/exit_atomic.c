5239a5716d49bb3420994ac42807477d2909510a
Aoto Tokizawa <aoto.tokizawa@gmail.com>
2019-11-23 21:04:56 +0000

    sed -i -e 's/void QEMU_NORETURN cpu_loop_exit_atomic(CPUState \*cpu, uintptr_t pc);/static void cpu_loop_exit_atomic(CPUState \*cpu, uintptr_t pc) {}/g' lib/arch/aarch64/helpers/*.c

--- lib/arch/aarch64/helpers/exit_atomic.c
+++ lib/arch/aarch64/helpers/exit_atomic.c
@@ -1178,7 +1178,7 @@ static inline CPUState *env_cpu(CPUArchState *env)
 
 #define HELPER(name) glue(helper_, name)
 
-void QEMU_NORETURN cpu_loop_exit_atomic(CPUState *cpu, uintptr_t pc);
+static void cpu_loop_exit_atomic(CPUState *cpu, uintptr_t pc) {}
 
 # define GETPC() \
     ((uintptr_t)__builtin_extract_return_addr(__builtin_return_address(0)))
