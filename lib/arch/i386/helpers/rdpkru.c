487845e5b61831fc14628c1b3370a4abfda92785
Aoto Tokizawa <aoto.tokizawa@gmail.com>
2019-11-29 19:32:05 -0500

    helpers/x86: Update

--- lib/arch/i386/helpers/rdpkru.c
+++ lib/arch/i386/helpers/rdpkru.c
@@ -565,8 +565,11 @@ void cpu_x86_cpuid(CPUX86State *env, uint32_t index, uint32_t count,
 
 #define SVM_EXIT_CPUID		0x072
 
-void QEMU_NORETURN raise_exception_err_ra(CPUX86State *env, int exception_index,
-                                          int error_code, uintptr_t retaddr);
+static void QEMU_NORETURN raise_exception_err_ra(CPUX86State *env, int exception_index,
+                                          int error_code, uintptr_t retaddr) {
+    __builtin_trap();
+    __builtin_unreachable();
+}
 
 void cpu_svm_check_intercept_param(CPUX86State *env1, uint32_t type,
                                    uint64_t param, uintptr_t retaddr);
--- lib/arch/i386/helpers/wrpkru.c
+++ lib/arch/i386/helpers/wrpkru.c
@@ -1030,8 +1030,11 @@ static inline CPUState *env_cpu(CPUArchState *env)
 
 #define SVM_EXIT_CPUID		0x072
 
-void QEMU_NORETURN raise_exception_err_ra(CPUX86State *env, int exception_index,
-                                          int error_code, uintptr_t retaddr);
+static void QEMU_NORETURN raise_exception_err_ra(CPUX86State *env, int exception_index,
+                                          int error_code, uintptr_t retaddr) {
+    __builtin_trap();
+    __builtin_unreachable();
+}
 
 void cpu_svm_check_intercept_param(CPUX86State *env1, uint32_t type,
                                    uint64_t param, uintptr_t retaddr);
