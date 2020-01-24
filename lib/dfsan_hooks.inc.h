deb1d49abd550fe17c52ae4d2b234a75e43254de
Aoto Tokizawa <aoto.tokizawa@gmail.com>
2020-01-24 16:30:44 -0500

    dfsan_hooks.inc: realloc

--- lib/dfsan_hooks.inc.h
+++ lib/dfsan_hooks.inc.h
@@ -1,3 +1,4 @@
 ___HOOK1(POST, void *, malloc, size_t)
 ___HOOK2(POST, void *, calloc, size_t, size_t)
 ___HOOK2(POST, char *, strchr, const char *, int)
+___HOOK2(POST, void *, realloc, void *, size_t)
