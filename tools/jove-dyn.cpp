9838b2583df1e91ea23fcf2e8aca2e6895aa8d08
Aoto Tokizawa <aoto.tokizawa@gmail.com>
2020-03-09 15:49:50 -0400

    dyn: on ArchLinux (x86_64), the kernel updated to have the [vsyscall] page present

--- tools/jove-dyn.cpp
+++ tools/jove-dyn.cpp
@@ -2589,6 +2589,8 @@ void search_address_space_for_binaries(pid_t child, disas_t &dis) {
     if (vm_prop.nm[0] != '/') {
       if (vm_prop.nm.find("[stack]") != std::string::npos)
         continue;
+      if (vm_prop.nm.find("[vsyscall]") != std::string::npos)
+        continue; /* if a dynamic target is in [vsyscall], we'll know */
     }
     if (vm_prop.nm.find("libjove_dyn_preload.so") != std::string::npos)
       continue;
