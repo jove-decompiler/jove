e2c306440daa99b92b047c4feb27de3c19268667
Aoto Tokizawa <aoto.tokizawa@gmail.com>
2020-04-21 18:33:43 -0400

    helpers/rdpmc: forgot these register definitions

--- lib/arch/x86_64/helpers/rdpmc.c
+++ lib/arch/x86_64/helpers/rdpmc.c
@@ -108,6 +108,34 @@ struct CPUWatchpoint {
 
 typedef uint64_t target_ulong;
 
+enum {
+    R_EAX = 0,
+    R_ECX = 1,
+    R_EDX = 2,
+    R_EBX = 3,
+    R_ESP = 4,
+    R_EBP = 5,
+    R_ESI = 6,
+    R_EDI = 7,
+    R_R8 = 8,
+    R_R9 = 9,
+    R_R10 = 10,
+    R_R11 = 11,
+    R_R12 = 12,
+    R_R13 = 13,
+    R_R14 = 14,
+    R_R15 = 15,
+
+    R_AL = 0,
+    R_CL = 1,
+    R_DL = 2,
+    R_BL = 3,
+    R_AH = 4,
+    R_CH = 5,
+    R_DH = 6,
+    R_BH = 7,
+};
+
 #define HF_CPL_SHIFT         0
 
 #define HF_CPL_MASK          (3 << HF_CPL_SHIFT)
