47264d0503210eaf00b383806592ecff10299a65
Aoto Tokizawa <aoto.tokizawa@gmail.com>
2020-12-31 18:29:39 -0500

    bootstrap: if _r_debug is not an exported symbol in the dynamic linker, try _dl_debug_addr

--- tools/jove-bootstrap.cpp
+++ tools/jove-bootstrap.cpp
@@ -67,6 +67,26 @@
 #include <boost/serialization/set.hpp>
 #include <boost/serialization/vector.hpp>
 
+static void __warn(const char *file, int line);
+
+#ifndef WARN
+#define WARN()                                                                 \
+  do {                                                                         \
+    __warn(__FILE__, __LINE__);                                                \
+  } while (0)
+#endif
+
+#ifndef WARN_ON
+#define WARN_ON(condition)                                                     \
+  ({                                                                           \
+    int __ret_warn_on = !!(condition);                                         \
+    if (unlikely(__ret_warn_on))                                               \
+      WARN();                                                                  \
+    unlikely(__ret_warn_on);                                                   \
+  })
+#endif
+
+
 extern "C" unsigned long getauxval(unsigned long type);
 
 #define GET_INSTRINFO_ENUM
@@ -4353,8 +4373,11 @@ void on_dynamic_linker_loaded(pid_t child,
       continue;
 
     llvm::StringRef SymName = unwrapOrError(Sym.getName(DynamicStringTable));
-    if (SymName != "_r_debug")
+    if (SymName != "_r_debug" &&
+        SymName != "_dl_debug_addr")
       continue;
+
+    WARN_ON(Sym.getType() != llvm::ELF::STT_OBJECT);
 
     _r_debug.Addr = va_of_rva(Sym.st_value, BIdx);
     _r_debug.Found = true;
@@ -4752,5 +4775,8 @@ std::pair<void *, unsigned> GetVDSO(void) {
   return std::make_pair(res.first, res.second);
 }
 
+} // namespace jove
 
+void __warn(const char *file, int line) {
+  WithColor::warning() << llvm::formatv("{0}:{1}\n", file, line);
 }
