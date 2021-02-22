a9b2e1cdc5301cd8f0f29d2e487b54f926ea39ec
Aoto Tokizawa <aoto.tokizawa@gmail.com>
2021-02-22 13:28:39 -0500

    remove the repository before the snap. it will continue to exist in git history (tag v0.7)

--- .old/include/coff_recompiler.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "recompiler.h"
-
-namespace jove {
-
-std::unique_ptr<recompiler>
-create_coff_recompiler(const llvm::object::ObjectFile &, llvm::Module &);
-
-}
--- .old/tests/src/librand.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-extern unsigned long librand_seed;
-unsigned long librand_get(void);
-void librand_dump_seed(void);
