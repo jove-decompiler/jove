ea36735a7ad9b53a1b3d7b1ec355ac2634327991
Aoto Tokizawa <aoto.tokizawa@gmail.com>
2021-03-09 17:02:57 -0500

    jove.c (mipsel): bring in callstack code

--- lib/arch/mipsel/jove.c
+++ lib/arch/mipsel/jove.c
@@ -1865,6 +1865,9 @@ _NOINL void _jove_check_return_address(target_ulong RetAddr,
 _HIDDEN target_ulong _jove_alloc_stack(void);
 _HIDDEN void _jove_free_stack(target_ulong);
 
+_HIDDEN target_ulong _jove_alloc_callstack(void);
+_HIDDEN void _jove_free_callstack(target_ulong);
+
 #define JOVE_CALLSTACK_SIZE (32 * JOVE_PAGE_SIZE)
 
 //
@@ -2218,38 +2221,9 @@ void _jove_trace_init(void) {
 }
 
 void _jove_callstack_init(void) {
-  if (__jove_callstack)
-    return;
+  uintptr_t ptr = _jove_alloc_callstack();
 
-  {
-    long ret =
-        _jove_sys_mips_mmap(0x0, JOVE_CALLSTACK_SIZE, PROT_READ | PROT_WRITE,
-                            MAP_PRIVATE | MAP_ANONYMOUS, -1L, 0);
-    if (ret < 0 && ret > -4096) {
-      __builtin_trap();
-      __builtin_unreachable();
-    }
-
-    void *ptr = (void *)ret;
-
-    //
-    // create guard pages on both sides
-    //
-    unsigned long beg = (unsigned long)ret;
-    unsigned long end = beg + JOVE_CALLSTACK_SIZE;
-
-    if (_jove_sys_mprotect(beg, JOVE_PAGE_SIZE, PROT_NONE) < 0) {
-      __builtin_trap();
-      __builtin_unreachable();
-    }
-
-    if (_jove_sys_mprotect(end - JOVE_PAGE_SIZE, JOVE_PAGE_SIZE, PROT_NONE) < 0) {
-      __builtin_trap();
-      __builtin_unreachable();
-    }
-
-    __jove_callstack_begin = __jove_callstack = ptr + JOVE_PAGE_SIZE;
-  }
+  __jove_callstack_begin = __jove_callstack = (void *)(ptr + JOVE_PAGE_SIZE);
 }
 
 static void _jove_flush_trace(void);
@@ -2944,6 +2918,42 @@ target_ulong _jove_alloc_stack(void) {
 
 void _jove_free_stack(target_ulong beg) {
   if (_jove_sys_munmap(beg, JOVE_STACK_SIZE) < 0) {
+    __builtin_trap();
+    __builtin_unreachable();
+  }
+}
+
+target_ulong _jove_alloc_callstack(void) {
+  long ret = _jove_sys_mips_mmap(0x0, JOVE_CALLSTACK_SIZE, PROT_READ | PROT_WRITE,
+                                 MAP_PRIVATE | MAP_ANONYMOUS, -1L, 0);
+  if (ret < 0 && ret > -4096) {
+    __builtin_trap();
+    __builtin_unreachable();
+  }
+
+  unsigned long uret = (unsigned long)ret;
+
+  //
+  // create guard pages on both sides
+  //
+  unsigned long beg = uret;
+  unsigned long end = beg + JOVE_CALLSTACK_SIZE;
+
+  if (_jove_sys_mprotect(beg, JOVE_PAGE_SIZE, PROT_NONE) < 0) {
+    __builtin_trap();
+    __builtin_unreachable();
+  }
+
+  if (_jove_sys_mprotect(end - JOVE_PAGE_SIZE, JOVE_PAGE_SIZE, PROT_NONE) < 0) {
+    __builtin_trap();
+    __builtin_unreachable();
+  }
+
+  return beg;
+}
+
+void _jove_free_callstack(target_ulong start) {
+  if (_jove_sys_munmap(start - JOVE_PAGE_SIZE /* XXX */, JOVE_CALLSTACK_SIZE) < 0) {
     __builtin_trap();
     __builtin_unreachable();
   }
