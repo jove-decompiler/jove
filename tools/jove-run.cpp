cf05ea216e8e9e420a40398af1e15493c92ae07c
Aoto Tokizawa <aoto.tokizawa@gmail.com>
2021-05-17 22:01:03 -0400

    run: in --sleep mode, sleep one second at a time- printing a single character after each second

--- tools/jove-run.cpp
+++ tools/jove-run.cpp
@@ -811,7 +811,10 @@ int run_outside_chroot(void) {
 
   if (unsigned sec = opts::Sleep) {
     fprintf(stderr, "sleeping for %u seconds...\n", sec);
-    sleep(sec);
+    for (unsigned t = 0; t < sec; ++t) {
+      sleep(1);
+      fprintf(stderr, "%s", ".");
+    }
   }
 
   //
