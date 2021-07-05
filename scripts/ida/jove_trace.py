e55ffd77bfc4bad2239eee8f094d7b876ebc1757
Aoto Tokizawa <aoto.tokizawa@gmail.com>
2021-07-05 11:59:55 -0700

    scripts/ida/jove_trace.py: first working ver

--- scripts/ida/jove_trace.py
+++ scripts/ida/jove_trace.py
@@ -8,6 +8,8 @@ Then you will be greeted with 2 file open dialogs, for the trace and jv
 Hotkeys:
 Ctrl+Shift+B - Previous block in trace
 Ctrl+Shift+N - Next block in trace
+Ctrl+Shift+V - Jump 1% behind current position in trace
+Ctrl+Shift+M - Jump 1% ahead of current position in trace
 """
 
 import subprocess
@@ -16,22 +18,22 @@ import xml.etree.cElementTree as ET
 
 ida_kernwin.create_menu("JoveToplevelMenu", "Jove", "View")
 
-JoveTrace = None
-decompilation = None
 bbaddrs = []
 trace = []
+pos = 0
 
 class jove_open_trace_file_t(ida_kernwin.action_handler_t):
     def __init__(self):
         ida_kernwin.action_handler_t.__init__(self)
 
     def activate(self, ctx):
-        global decompilation 
         global bbaddrs
         global trace
+        global pos
 
         bbaddrs.clear()
         trace.clear()
+        pos = 0
 
         jv_fn = ida_kernwin.ask_file(0, "*.jv", "Load decompilation")
         if not jv_fn:
@@ -86,7 +88,14 @@ class jove_open_trace_file_t(ida_kernwin.action_handler_t):
         with open(trace_fn) as f:
             lines = f.readlines()
             for line in lines:
-                print("line: \"%s\"" % line);
+                prefix = "JV_0_"
+
+                if len(line) < len(prefix) or line.find(prefix) == -1:
+                    continue
+
+                bbidx = int(line[len(prefix):])
+                print("bbidx: %d" % bbidx);
+                trace.append(bbidx)
 
     def update(self, ctx):
         return ida_kernwin.AST_ENABLE_ALWAYS
@@ -96,10 +105,18 @@ class jove_next_trace_block_t(ida_kernwin.action_handler_t):
         ida_kernwin.action_handler_t.__init__(self)
 
     def activate(self, ctx):
-        if JoveTrace is None:
+        global trace
+        global pos
+
+        if trace is None:
             print("jove_next_trace_block_t::activate: error: no trace file opened")
             return
-        print("Next Block Address");
+
+        pos = min(pos + 1, len(trace) - 1)
+
+        Addr = bbaddrs[trace[pos]]
+        print("Block @ 0x%x (%d / %d)" % (Addr, pos, len(trace)));
+        ida_kernwin.jumpto(Addr)
 
     def update(self, ctx):
         return ida_kernwin.AST_ENABLE_ALWAYS
@@ -109,10 +126,64 @@ class jove_prev_trace_block_t(ida_kernwin.action_handler_t):
         ida_kernwin.action_handler_t.__init__(self)
 
     def activate(self, ctx):
-        if JoveTrace is None:
+        global trace
+        global pos
+
+        if trace is None:
             print("jove_prev_trace_block_t::activate: error: no trace file opened")
             return
-        print("Previous Block Address");
+
+        pos = max(pos - 1, 0)
+
+        Addr = bbaddrs[trace[pos]]
+        print("Block @ 0x%x (%d / %d)" % (Addr, pos, len(trace)));
+        ida_kernwin.jumpto(Addr)
+
+    def update(self, ctx):
+        return ida_kernwin.AST_ENABLE_ALWAYS
+
+class jove_skip_ahead_t(ida_kernwin.action_handler_t):
+    def __init__(self):
+        ida_kernwin.action_handler_t.__init__(self)
+
+    def activate(self, ctx):
+        global trace
+        global pos
+
+        if trace is None:
+            print("jove_skip_ahead_t::activate: error: no trace file opened")
+            return
+
+        x = float(pos) / float(len(trace));
+        x = min(x + 0.01, 1.0)
+        pos = min(int(x * len(trace)), len(trace) - 1)
+
+        Addr = bbaddrs[trace[pos]]
+        print("Basic Block @ 0x%x (%d / %d)" % (Addr, pos, len(trace)));
+        ida_kernwin.jumpto(Addr)
+
+    def update(self, ctx):
+        return ida_kernwin.AST_ENABLE_ALWAYS
+
+class jove_skip_behind_t(ida_kernwin.action_handler_t):
+    def __init__(self):
+        ida_kernwin.action_handler_t.__init__(self)
+
+    def activate(self, ctx):
+        global trace
+        global pos
+
+        if trace is None:
+            print("jove_skip_behind_t::activate: error: no trace file opened")
+            return
+
+        x = float(pos) / float(len(trace));
+        x = max(x - 0.01, 0.0)
+        pos = min(int(x * len(trace)), len(trace) - 1)
+
+        Addr = bbaddrs[trace[pos]]
+        print("Block @ 0x%x (%d / %d)" % (Addr, pos, len(trace)));
+        ida_kernwin.jumpto(Addr)
 
     def update(self, ctx):
         return ida_kernwin.AST_ENABLE_ALWAYS
@@ -120,13 +191,19 @@ class jove_prev_trace_block_t(ida_kernwin.action_handler_t):
 ACTION_0_NAME = "jove_action_open_trace"
 ACTION_1_NAME = "jove_action_trace_next"
 ACTION_2_NAME = "jove_action_trace_prev"
+ACTION_3_NAME = "jove_action_skip_ahead"
+ACTION_4_NAME = "jove_action_skip_behind"
 
 ACTION_1_SHORTCUT = "Ctrl+Shift+N"
 ACTION_2_SHORTCUT = "Ctrl+Shift+B"
+ACTION_3_SHORTCUT = "Ctrl+Shift+M"
+ACTION_4_SHORTCUT = "Ctrl+Shift+V"
 
 desc_0 = ida_kernwin.action_desc_t(ACTION_0_NAME, "Open trace...", jove_open_trace_file_t())
 desc_1 = ida_kernwin.action_desc_t(ACTION_1_NAME, "Jump to next block in trace", jove_next_trace_block_t(), ACTION_1_SHORTCUT)
 desc_2 = ida_kernwin.action_desc_t(ACTION_2_NAME, "Jump to previous block in trace", jove_prev_trace_block_t(), ACTION_2_SHORTCUT)
+desc_3 = ida_kernwin.action_desc_t(ACTION_3_NAME, "Skip ahead", jove_skip_ahead_t(), ACTION_3_SHORTCUT)
+desc_4 = ida_kernwin.action_desc_t(ACTION_4_NAME, "Skip behind", jove_skip_behind_t(), ACTION_4_SHORTCUT)
 
 if ida_kernwin.register_action(desc_0):
     ida_kernwin.attach_action_to_menu("Jove", ACTION_0_NAME, ida_kernwin.SETMENU_INS)
@@ -142,3 +219,13 @@ if ida_kernwin.register_action(desc_2):
     ida_kernwin.attach_action_to_menu("Jove", ACTION_2_NAME, ida_kernwin.SETMENU_INS)
 else:
     print("Failed to register action \"%s\"" % ACTION_2_NAME)
+
+if ida_kernwin.register_action(desc_3):
+    ida_kernwin.attach_action_to_menu("Jove", ACTION_3_NAME, ida_kernwin.SETMENU_INS)
+else:
+    print("Failed to register action \"%s\"" % ACTION_3_NAME)
+
+if ida_kernwin.register_action(desc_4):
+    ida_kernwin.attach_action_to_menu("Jove", ACTION_4_NAME, ida_kernwin.SETMENU_INS)
+else:
+    print("Failed to register action \"%s\"" % ACTION_4_NAME)
