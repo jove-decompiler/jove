475c87531b63b573e4c4406de29a51379a392639
Aoto Tokizawa <aoto.tokizawa@gmail.com>
2021-07-06 20:13:58 -0700

    scripts/ida/jove_view_trace: ask the user *once* for the path to the jv file, immediately after the script is loaded

--- scripts/ida/jove_view_trace.py
+++ scripts/ida/jove_view_trace.py
@@ -18,6 +18,7 @@ import xml.etree.cElementTree as ET
 
 ida_kernwin.create_menu("JoveToplevelMenu", "Jove", "View")
 
+decompilation = None
 bbaddrs = []
 trace = []
 pos = 0
@@ -27,20 +28,14 @@ class jove_open_trace_file_t(ida_kernwin.action_handler_t):
         ida_kernwin.action_handler_t.__init__(self)
 
     def activate(self, ctx):
+        global decompilation
         global bbaddrs
         global trace
         global pos
 
-        jv_fn = ida_kernwin.ask_file(0, "*.jv", "Load decompilation")
-        if not jv_fn:
+        if not decompilation:
+            print("couldn't open decompilation")
             return
-
-        print("parsing decompilation...")
-
-        jv2xml_path = subprocess.Popen(["/usr/bin/which", "jv2xml"], stdout=subprocess.PIPE).communicate()[0].strip()
-        xml = subprocess.Popen([jv2xml_path, jv_fn], stdout=subprocess.PIPE).communicate()[0]
-
-        decompilation = ET.ElementTree(ET.fromstring(xml))
 
         e = decompilation.getroot()
 
@@ -140,7 +135,7 @@ class jove_prev_trace_block_t(ida_kernwin.action_handler_t):
         pos = max(pos - 1, 0)
 
         Addr = bbaddrs[trace[pos]]
-        print("Block @ 0x%x (%d / %d)" % (Addr, pos, len(trace)));
+        print("Block @ 0x%x (%d / %d)" % (Addr, pos + 1, len(trace)));
         ida_kernwin.jumpto(Addr)
 
     def update(self, ctx):
@@ -154,7 +149,7 @@ class jove_skip_ahead_t(ida_kernwin.action_handler_t):
         global trace
         global pos
 
-        if trace is None:
+        if len(trace) == 0:
             print("[jove] no trace file opened?")
             return
 
@@ -177,7 +172,7 @@ class jove_skip_behind_t(ida_kernwin.action_handler_t):
         global trace
         global pos
 
-        if trace is None:
+        if len(trace) == 0:
             print("[jove] no trace file opened?")
             return
 
@@ -192,44 +187,55 @@ class jove_skip_behind_t(ida_kernwin.action_handler_t):
     def update(self, ctx):
         return ida_kernwin.AST_ENABLE_ALWAYS
 
-ACTION_0_NAME = "jove_action_open_trace"
-ACTION_1_NAME = "jove_action_trace_next"
-ACTION_2_NAME = "jove_action_trace_prev"
-ACTION_3_NAME = "jove_action_skip_ahead"
-ACTION_4_NAME = "jove_action_skip_behind"
-
-ACTION_1_SHORTCUT = "Ctrl+Shift+N"
-ACTION_2_SHORTCUT = "Ctrl+Shift+B"
-ACTION_3_SHORTCUT = "Ctrl+Shift+M"
-ACTION_4_SHORTCUT = "Ctrl+Shift+V"
-
-desc_0 = ida_kernwin.action_desc_t(ACTION_0_NAME, "Open trace...", jove_open_trace_file_t())
-desc_1 = ida_kernwin.action_desc_t(ACTION_1_NAME, "Jump to next block in trace", jove_next_trace_block_t(), ACTION_1_SHORTCUT)
-desc_2 = ida_kernwin.action_desc_t(ACTION_2_NAME, "Jump to previous block in trace", jove_prev_trace_block_t(), ACTION_2_SHORTCUT)
-desc_3 = ida_kernwin.action_desc_t(ACTION_3_NAME, "Skip ahead", jove_skip_ahead_t(), ACTION_3_SHORTCUT)
-desc_4 = ida_kernwin.action_desc_t(ACTION_4_NAME, "Skip behind", jove_skip_behind_t(), ACTION_4_SHORTCUT)
-
-if ida_kernwin.register_action(desc_0):
-    ida_kernwin.attach_action_to_menu("Jove", ACTION_0_NAME, ida_kernwin.SETMENU_INS)
+jv_fn = ida_kernwin.ask_file(0, "*.jv", "Load decompilation")
+if not jv_fn:
+    pass
 else:
-    print("Failed to register action \"%s\"" % ACTION_0_NAME)
+    print("parsing decompilation...")
 
-if ida_kernwin.register_action(desc_1):
-    ida_kernwin.attach_action_to_menu("Jove", ACTION_1_NAME, ida_kernwin.SETMENU_INS)
-else:
-    print("Failed to register action \"%s\"" % ACTION_1_NAME)
+    jv2xml_path = subprocess.Popen(["/usr/bin/which", "jv2xml"], stdout=subprocess.PIPE).communicate()[0].strip()
+    xml = subprocess.Popen([jv2xml_path, jv_fn], stdout=subprocess.PIPE).communicate()[0]
 
-if ida_kernwin.register_action(desc_2):
-    ida_kernwin.attach_action_to_menu("Jove", ACTION_2_NAME, ida_kernwin.SETMENU_INS)
-else:
-    print("Failed to register action \"%s\"" % ACTION_2_NAME)
+    decompilation = ET.ElementTree(ET.fromstring(xml))
 
-if ida_kernwin.register_action(desc_3):
-    ida_kernwin.attach_action_to_menu("Jove", ACTION_3_NAME, ida_kernwin.SETMENU_INS)
-else:
-    print("Failed to register action \"%s\"" % ACTION_3_NAME)
+    ACTION_0_NAME = "jove_action_open_trace"
+    ACTION_1_NAME = "jove_action_trace_next"
+    ACTION_2_NAME = "jove_action_trace_prev"
+    ACTION_3_NAME = "jove_action_skip_ahead"
+    ACTION_4_NAME = "jove_action_skip_behind"
 
-if ida_kernwin.register_action(desc_4):
-    ida_kernwin.attach_action_to_menu("Jove", ACTION_4_NAME, ida_kernwin.SETMENU_INS)
-else:
-    print("Failed to register action \"%s\"" % ACTION_4_NAME)
+    ACTION_1_SHORTCUT = "Ctrl+Shift+N"
+    ACTION_2_SHORTCUT = "Ctrl+Shift+B"
+    ACTION_3_SHORTCUT = "Ctrl+Shift+M"
+    ACTION_4_SHORTCUT = "Ctrl+Shift+V"
+
+    desc_0 = ida_kernwin.action_desc_t(ACTION_0_NAME, "Open trace...", jove_open_trace_file_t())
+    desc_1 = ida_kernwin.action_desc_t(ACTION_1_NAME, "Jump to next block in trace", jove_next_trace_block_t(), ACTION_1_SHORTCUT)
+    desc_2 = ida_kernwin.action_desc_t(ACTION_2_NAME, "Jump to previous block in trace", jove_prev_trace_block_t(), ACTION_2_SHORTCUT)
+    desc_3 = ida_kernwin.action_desc_t(ACTION_3_NAME, "Skip ahead", jove_skip_ahead_t(), ACTION_3_SHORTCUT)
+    desc_4 = ida_kernwin.action_desc_t(ACTION_4_NAME, "Skip behind", jove_skip_behind_t(), ACTION_4_SHORTCUT)
+
+    if ida_kernwin.register_action(desc_0):
+        ida_kernwin.attach_action_to_menu("Jove", ACTION_0_NAME, ida_kernwin.SETMENU_INS)
+    else:
+        print("Failed to register action \"%s\"" % ACTION_0_NAME)
+
+    if ida_kernwin.register_action(desc_1):
+        ida_kernwin.attach_action_to_menu("Jove", ACTION_1_NAME, ida_kernwin.SETMENU_INS)
+    else:
+        print("Failed to register action \"%s\"" % ACTION_1_NAME)
+
+    if ida_kernwin.register_action(desc_2):
+        ida_kernwin.attach_action_to_menu("Jove", ACTION_2_NAME, ida_kernwin.SETMENU_INS)
+    else:
+        print("Failed to register action \"%s\"" % ACTION_2_NAME)
+
+    if ida_kernwin.register_action(desc_3):
+        ida_kernwin.attach_action_to_menu("Jove", ACTION_3_NAME, ida_kernwin.SETMENU_INS)
+    else:
+        print("Failed to register action \"%s\"" % ACTION_3_NAME)
+
+    if ida_kernwin.register_action(desc_4):
+        ida_kernwin.attach_action_to_menu("Jove", ACTION_4_NAME, ida_kernwin.SETMENU_INS)
+    else:
+        print("Failed to register action \"%s\"" % ACTION_4_NAME)
