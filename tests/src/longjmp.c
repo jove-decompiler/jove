c5a14aa651541bf207c63bef142e6c691d21494f
Aoto Tokizawa <aoto.tokizawa@gmail.com>
2022-01-14 12:48:30 -0500

    tests/longjmp.c: initial commit

---
+++ tests/src/longjmp.c
@@ -0,0 +1,23 @@
+#include <setjmp.h>
+#include <stdio.h>
+
+static jmp_buf env;
+
+int foo(int n) {
+  if (n % 10 == 1) {
+    longjmp(env, 1);
+  }
+
+  return 0;
+}
+
+int main(int argc, char **argv) {
+  int n = argc;
+
+  if (setjmp(env) == 1) {
+    printf("longjumped! n=%d\n", n);
+    return 1;
+  }
+
+  return foo(n);
+}
