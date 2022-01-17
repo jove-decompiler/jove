jove-decompiler/jove

commit 2d6e2899a706dbc671161a6d31ba1d2147720350

llvm: for IFunc resolvers that we create, don't return a pointer to the callable version of the dynamic target we have from jove-bootstrap. return a section pointer, so that we can eliminate checking the program counter equals the section pointer *OR* the callable address, and instead just look at the section pointers.

tools/jove-llvm.cpp
@@ -2183,12 +2183,12 @@ llvm::GlobalIFunc *buildGlobalIFunc(function_t &f, dynamic_target_t IdxPair) {
       IRB.CreateCall(JoveInstallForeignFunctionTables)
           ->setIsNoInline();
 
-      llvm::Value *Res = GetDynTargetAddress<true>(IRB, IdxPair);
+      llvm::Value *Res = GetDynTargetAddress<false>(IRB, IdxPair);
 
       IRB.CreateRet(IRB.CreateIntToPtr(
           Res, CallsF->getFunctionType()->getReturnType()));
     } else if (is_dynamic_target_valid(IdxPair) && !Decompilation.Binaries.at(IdxPair.first).IsDynamicallyLoaded) {
-      llvm::Value *Res = GetDynTargetAddress<true>(IRB, IdxPair);
+      llvm::Value *Res = GetDynTargetAddress<false>(IRB, IdxPair);
 
       IRB.CreateRet(IRB.CreateIntToPtr(
           Res, CallsF->getFunctionType()->getReturnType()));

commit d17639ca157352e19bee61e834fb719248e0dda6

llvm: fix regression caused by 2d6e2899a706dbc671161a6d31ba1d2147720350- we are still creating IFuncs that return the callable address of a function, not the section pointer

tools/jove-llvm.cpp
@@ -2178,6 +2178,13 @@ llvm::GlobalIFunc *buildGlobalIFunc(function_t &f, dynamic_target_t IdxPair) {
               .F,
           WordType());
 
+      //
+      // FIXME we can't only look at the SectionPointer when translating an
+      // indirect jump because of this. Unfortunately, at the moment we cannot
+      // just return SectionPointer() here because this function is called
+      // *before* CreateSectionGlobalVariables.
+      //
+
       IRB.CreateRet(IRB.CreateIntToPtr(
           Res, CallsF->getFunctionType()->getReturnType()));
     } else if (is_dynamic_target_valid(IdxPair) && DynTargetNeedsThunkPred(IdxPair)) {
@@ -8063,9 +8070,18 @@ int TranslateBasicBlock(TranslateContext &TC) {
                 (fmt("if %s") % dyn_target_desc(DynTargetsVec[next_i])).str(),
                 f.F);
 
+#if 0
           llvm::Value *EQVal = IRB.CreateICmpEQ(
               PC, GetDynTargetAddress<false>(IRB, DynTargetsVec[i], B));
           IRB.CreateCondBr(EQVal, DynTargetsDoCallBVec[i], B);
+#else
+          llvm::Value *EQV_1 = IRB.CreateICmpEQ(
+              PC, GetDynTargetAddress<false>(IRB, DynTargetsVec[i], B));
+          llvm::Value *EQV_2 = IRB.CreateICmpEQ(
+              PC, GetDynTargetAddress<true>(IRB, DynTargetsVec[i], B));
+
+          IRB.CreateCondBr(IRB.CreateOr(EQV_1, EQV_2), DynTargetsDoCallBVec[i], B);
+#endif
         } while (++i != DynTargetsVec.size());
 
         ElseB = B;