d6ace947bdf49ae067b0b9c3e11f29ab5c805446
Aoto Tokizawa <aoto.tokizawa@gmail.com>
2023-07-03 21:49:43 -0400

    build.sh: symlink may have already been created

--- scripts/build/build.sh
+++ scripts/build/build.sh
@@ -8,6 +8,9 @@ jove_path=$build_scripts_path/../..
 qemu_path=$jove_path/qemu
 llvm_path=$jove_path/llvm-project
 
+rm -f $llvm_path/llvm/projects/jove
+rm -f $llvm_path/llvm/projects/llvm-cbe
+
 ln -sf ../../.. $llvm_path/llvm/projects/jove
 ln -sf ../../../llvm-cbe $llvm_path/llvm/projects/llvm-cbe
 
