d6fd44a2d6d3f55f093d752caf153ee433d87bf5
Aoto Tokizawa <aoto.tokizawa@gmail.com>
2023-08-09 00:47:13 +0000

    tests/run.py: --preexisting

--- tests/run.py
+++ tests/run.py
@@ -7,64 +7,83 @@ import pathlib
 import argparse
 import os
 import time
+import re
 
 parser = argparse.ArgumentParser(description='Run tests.')
 parser.add_argument('tests', metavar='T', type=str, nargs='+', help='tests to execute')
 parser.add_argument('-a', dest='arch', type=str, required=True, help='specify architecture')
+parser.add_argument('--preexisting', dest='preexisting', type=str, help='specify preexisting directory')
 
 args = parser.parse_args()
-print(args)
 
-tests_dir = pathlib.Path(__file__).parent.resolve()
+td = tempfile.TemporaryDirectory()
+d = td.name
 
-d = tempfile.TemporaryDirectory()
-print(d.name)
+if args.preexisting is None:
+  tests_dir = pathlib.Path(__file__).parent.resolve()
+  bringup_path = '%s/../mk-deb-vm/bringup.sh' % str(tests_dir)
 
-bringup_path = '%s/../mk-deb-vm/bringup.sh' % str(tests_dir)
-print(bringup_path)
+  subprocess.run(['sudo', bringup_path, '-a', args.arch, '-o', d, '-f'], check=True)
+else:
+  d = args.preexisting
 
-subprocess.run(['sudo', bringup_path, '-a', args.arch, '-o', d.name, '-f'], check=True)
+os.mkfifo('%s/x.in' % d)
+os.mkfifo('%s/x.out' % d)
 
-os.mkfifo('%s/x.in' % d.name)
-os.mkfifo('%s/x.out' % d.name)
+cp = subprocess.Popen(['cat', '%s/x.out' % d], stdout=open('%s/stdout.txt' % d, 'wb', buffering=0), stdin=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
-cp = subprocess.Popen(['cat', '%s/x.out' % d.name], stdout=open('%s/stdout.txt' % d.name, 'wb', buffering=0), stdin=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+os.chdir(d)
+qp = subprocess.Popen(['%s/run.sh' % d], stdin=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
 
-os.chdir(d.name)
-qp = subprocess.Popen(['%s/run.sh' % d.name], stdin=subprocess.DEVNULL, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-
-def serial_write(s):
-  if qp.poll() != None:
+def serial_write(s, t=0.5):
+  if qp.poll() != None or cp.poll() != None:
     return False
 
-  inf = open('%s/x.in' % d.name, 'wb', buffering=0)
+  inf = open('%s/x.in' % d, 'wb', buffering=0)
   inf.write(s.encode())
   inf.close()
+
+  time.sleep(t)
+
   return True
 
-while True:
+def strip_ansi_escape_sequences(s):
+  ansi_escape = re.compile(r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])')
+  return ansi_escape.sub('', s)
+
+def serial_tail(n=1):
+  completed_process = subprocess.run(['tail', '-n', str(n), '%s/stdout.txt' % d], capture_output=True, text=True)
+  return strip_ansi_escape_sequences(completed_process.stdout)
+
+#
+# wait for system to boot up
+#
+print("waiting for system to boot...")
+
+time.sleep(0.5)
+
+while not serial_tail().strip().endswith("login:"):
   assert(serial_write("\n"))
-  time.sleep(1)
-  tp = subprocess.run(['tail', '-n', '1', '%s/stdout.txt' % d.name], capture_output=True, text=True)
-  if tp.stdout.strip().endswith("login:"):
-    print("found login:")
-    break
 
-while True:
-  if serial_write("root\n"):
-    time.sleep(1)
-  else:
-    break
+print("system booted.")
 
-  if serial_write("root\n"):
-    time.sleep(3)
-  else:
-    break
+#
+# log in to system
+#
+print("logging into system...")
 
-  if serial_write("systemctl poweroff\n"):
-    time.sleep(10)
-  else:
-    break
+assert(serial_write("root\n", 0.8) and serial_write("root\n"))
+
+serial_write("ip route show\n")
+print('\n\"%s\"\n' % serial_tail(5).strip())
+print(serial_tail(5).strip().split()[2])
+
+#
+# power off system
+#
+print("powering off system...")
+
+assert(serial_write("systemctl poweroff\n"))
 
 qp.communicate()
 cp.communicate()
