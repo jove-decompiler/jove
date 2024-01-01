72f9baf9e78040937cd2d3af687bdbfd959557a5
Aoto Tokizawa <aoto.tokizawa@gmail.com>
2024-01-01 03:51:39 +0000

    _jove_thunk (aarch64): pass and return floating point arguments (d_)

--- lib/arch/aarch64/jove.c
+++ lib/arch/aarch64/jove.c
@@ -169,14 +169,8 @@ void _jove_begin(uint64_t x0,
 /*"stp x23, x24, [sp, #48]\n"*/                                                \
 /*"stp x25, x26, [sp, #64]\n"*/                                                \
 /*"stp x27, x28, [sp, #80]\n"*/                                                \
-                                                                               \
-  "mov x22, sp\n" /* save sp in x22 */
 
 #define JOVE_THUNK_EPILOGUE                                                    \
-  "mov x9, sp\n" /* store modified emusp */                                    \
-  "str x9, [x21]\n"                                                            \
-                                                                               \
-  "mov sp, x22\n"   /* restore stack pointer */                                \
                                                                                \
 /*"ldp x19, x20, [sp, #16]\n"*//* callee-saved registers */                    \
   "ldp x21, x22, [sp, #32]\n"                                                  \
@@ -188,20 +182,56 @@ void _jove_begin(uint64_t x0,
                                                                                \
   "ret\n"
 
+#define JOVE_THUNK_EXTRA_ARGS                                                  \
+  "ldr d0, [x21, #2920]\n"                                                     \
+  "ldr d1, [x21, #3176]\n"                                                     \
+  "ldr d2, [x21, #3432]\n"                                                     \
+  "ldr d3, [x21, #3688]\n"                                                     \
+  "ldr d4, [x21, #3944]\n"                                                     \
+  "ldr d5, [x21, #4200]\n"                                                     \
+  "ldr d6, [x21, #4456]\n"                                                     \
+  "ldr d7, [x21, #4712]\n"
+
+#define JOVE_THUNK_EXTRA_RETS                                                  \
+  "str d0, [x21, #2920]\n"                                                     \
+  "str d1, [x21, #3176]\n"                                                     \
+  "str d2, [x21, #3432]\n"                                                     \
+  "str d3, [x21, #3688]\n"                                                     \
+  "str d4, [x21, #3944]\n"                                                     \
+  "str d5, [x21, #4200]\n"                                                     \
+  "str d6, [x21, #4456]\n"                                                     \
+  "str d7, [x21, #4712]\n"
+
+#define JOVE_THUNK_CORE                                                        \
+  JOVE_THUNK_EXTRA_ARGS                                                        \
+                                                                               \
+  "mov x22, sp\n" /* save sp in x22 */                                         \
+                                                                               \
+  "ldr x9, [x21]\n" /* sp=*emuspp */                                           \
+  "mov sp, x9\n"                                                               \
+  "str xzr, [x21]\n" /* *emuspp=NULL */                                        \
+                                                                               \
+  /* args: nothing to do */                                                    \
+                                                                               \
+  "blr x10\n" /* call dstpc */                                                 \
+                                                                               \
+  "mov x9, sp\n" /* store modified emusp */                                    \
+  "str x9, [x21]\n"                                                            \
+                                                                               \
+  "mov sp, x22\n" /* restore stack pointer */                                  \
+                                                                               \
+  JOVE_THUNK_EXTRA_RETS                                                        \
+                                                                               \
+  JOVE_THUNK_EPILOGUE
+
 jove_thunk_return_t _jove_thunk0(uint64_t dstpc   /* x0 */,
                                  uint64_t *emuspp /* x1 */) {
   asm volatile(JOVE_THUNK_PROLOGUE
 
+               "mov x10, x0\n" /* dstpc in x10 */
                "mov x21, x1\n" /* emuspp in x21 */
 
-               "ldr x9, [x1]\n" /* sp=*emuspp */
-               "mov sp, x9\n"
-               "str xzr, [x1]\n" /* *emuspp=NULL */
-
-               /* args: nothing to do */
-               "blr x0\n" /* call dstpc */
-
-               JOVE_THUNK_EPILOGUE
+               JOVE_THUNK_CORE
                : /* OutputOperands */
                : /* InputOperands */
                : /* Clobbers */);
@@ -212,16 +242,10 @@ jove_thunk_return_t _jove_thunk1(uint64_t x0,
                                  uint64_t *emuspp /* x2 */) {
   asm volatile(JOVE_THUNK_PROLOGUE
 
+               "mov x10, x1\n" /* dstpc in x10 */
                "mov x21, x2\n" /* emuspp in x21 */
 
-               "ldr x9, [x2]\n" /* sp=*emuspp */
-               "mov sp, x9\n"
-               "str xzr, [x2]\n" /* *emuspp=NULL */
-
-               /* args: nothing to do */
-               "blr x1\n" /* call dstpc */
-
-               JOVE_THUNK_EPILOGUE
+               JOVE_THUNK_CORE
                : /* OutputOperands */
                : /* InputOperands */
                : /* Clobbers */);
@@ -233,16 +257,10 @@ jove_thunk_return_t _jove_thunk2(uint64_t x0,
                                  uint64_t *emuspp /* x3 */) {
   asm volatile(JOVE_THUNK_PROLOGUE
 
+               "mov x10, x2\n" /* dstpc in x10 */
                "mov x21, x3\n" /* emuspp in x21 */
 
-               "ldr x9, [x3]\n" /* sp=*emuspp */
-               "mov sp, x9\n"
-               "str xzr, [x3]\n" /* *emuspp=NULL */
-
-               /* args: nothing to do */
-               "blr x2\n" /* call dstpc */
-
-               JOVE_THUNK_EPILOGUE
+               JOVE_THUNK_CORE
                : /* OutputOperands */
                : /* InputOperands */
                : /* Clobbers */);
@@ -255,16 +273,10 @@ jove_thunk_return_t _jove_thunk3(uint64_t x0,
                                  uint64_t *emuspp /* x4 */) {
   asm volatile(JOVE_THUNK_PROLOGUE
 
+               "mov x10, x3\n" /* dstpc in x10 */
                "mov x21, x4\n" /* emuspp in x21 */
 
-               "ldr x9, [x4]\n" /* sp=*emuspp */
-               "mov sp, x9\n"
-               "str xzr, [x4]\n" /* *emuspp=NULL */
-
-               /* args: nothing to do */
-               "blr x3\n" /* call dstpc */
-
-               JOVE_THUNK_EPILOGUE
+               JOVE_THUNK_CORE
                : /* OutputOperands */
                : /* InputOperands */
                : /* Clobbers */);
@@ -278,16 +290,10 @@ jove_thunk_return_t _jove_thunk4(uint64_t x0,
                                  uint64_t *emuspp /* x5 */) {
   asm volatile(JOVE_THUNK_PROLOGUE
 
+               "mov x10, x4\n" /* dstpc in x10 */
                "mov x21, x5\n" /* emuspp in x21 */
 
-               "ldr x9, [x5]\n" /* sp=*emuspp */
-               "mov sp, x9\n"
-               "str xzr, [x5]\n" /* *emuspp=NULL */
-
-               /* args: nothing to do */
-               "blr x4\n" /* call dstpc */
-
-               JOVE_THUNK_EPILOGUE
+               JOVE_THUNK_CORE
                : /* OutputOperands */
                : /* InputOperands */
                : /* Clobbers */);
@@ -302,16 +308,10 @@ jove_thunk_return_t _jove_thunk5(uint64_t x0,
                                  uint64_t *emuspp /* x6 */) {
   asm volatile(JOVE_THUNK_PROLOGUE
 
+               "mov x10, x5\n" /* dstpc in x10 */
                "mov x21, x6\n" /* emuspp in x21 */
 
-               "ldr x9, [x6]\n" /* sp=*emuspp */
-               "mov sp, x9\n"
-               "str xzr, [x6]\n" /* *emuspp=NULL */
-
-               /* args: nothing to do */
-               "blr x5\n" /* call dstpc */
-
-               JOVE_THUNK_EPILOGUE
+               JOVE_THUNK_CORE
                : /* OutputOperands */
                : /* InputOperands */
                : /* Clobbers */);
@@ -327,16 +327,10 @@ jove_thunk_return_t _jove_thunk6(uint64_t x0,
                                  uint64_t *emuspp /* x7 */) {
   asm volatile(JOVE_THUNK_PROLOGUE
 
+               "mov x10, x6\n" /* dstpc in x10 */
                "mov x21, x7\n" /* emuspp in x21 */
 
-               "ldr x9, [x7]\n" /* sp=*emuspp */
-               "mov sp, x9\n"
-               "str xzr, [x7]\n" /* *emuspp=NULL */
-
-               /* args: nothing to do */
-               "blr x6\n" /* call dstpc */
-
-               JOVE_THUNK_EPILOGUE
+               JOVE_THUNK_CORE
                : /* OutputOperands */
                : /* InputOperands */
                : /* Clobbers */);
@@ -353,16 +347,10 @@ jove_thunk_return_t _jove_thunk7(uint64_t x0,
                                  uint64_t *emuspp) {
   asm volatile(JOVE_THUNK_PROLOGUE
 
+               "mov x10, x7\n"         /* dstpc in x10 */
                "ldr x21, [sp, #128]\n" /* emuspp in x21 */
 
-               "ldr x9, [x21]\n" /* sp=*emuspp */
-               "mov sp, x9\n"
-               "str xzr, [x21]\n" /* *emuspp=NULL */
-
-               /* args: nothing to do */
-               "blr x7\n" /* call dstpc */
-
-               JOVE_THUNK_EPILOGUE
+               JOVE_THUNK_CORE
                : /* OutputOperands */
                : /* InputOperands */
                : /* Clobbers */);
@@ -380,17 +368,10 @@ jove_thunk_return_t _jove_thunk8(uint64_t x0,
                                  uint64_t *emuspp) {
   asm volatile(JOVE_THUNK_PROLOGUE
 
-               "ldr x21, [sp, #136]\n" /* emuspp in x21 */
                "ldr x10, [sp, #128]\n" /* dstpc in x10 */
+               "ldr x21, [sp, #136]\n" /* emuspp in x21 */
 
-               "ldr x9, [x21]\n" /* sp=*emuspp */
-               "mov sp, x9\n"
-               "str xzr, [x21]\n" /* *emuspp=NULL */
-
-               /* args: nothing to do */
-               "blr x10\n" /* call dstpc */
-
-               JOVE_THUNK_EPILOGUE
+               JOVE_THUNK_CORE
                : /* OutputOperands */
                : /* InputOperands */
                : /* Clobbers */);
