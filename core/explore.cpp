2e3a36cd07e59b33da34c03ad5da0ad2d547c6ba
Aoto Tokizawa <aoto.tokizawa@gmail.com>
2024-01-02 05:46:48 +0000

    style (consistency)

--- core/explore.cpp
+++ core/explore.cpp
@@ -164,9 +164,11 @@ on_insn_boundary:
         newbbprop.DynTargetsComplete = false;
         newbbprop.Term._call.Target = invalid_function_index;
         newbbprop.Term._call.Returns = false;
+        newbbprop.Term._call.ReturnsOff = 0;
         newbbprop.Term._indirect_jump.IsLj = false;
         newbbprop.Sj = false;
         newbbprop.Term._indirect_call.Returns = false;
+        newbbprop.Term._indirect_call.ReturnsOff = 0;
         newbbprop.Term._return.Returns = false;
         newbbprop.InvalidateAnalysis();
       }
