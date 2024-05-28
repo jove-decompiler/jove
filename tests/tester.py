75ff5907f17981eb9f816fa2e6234c7572a8f695
Aoto Tokizawa <aoto.tokizawa@gmail.com>
2024-05-28 05:30:50 +0000

    tests/tester.py: typo

--- tests/tester.py
+++ tests/tester.py
@@ -229,7 +229,7 @@ class JoveTester:
               print('<STDERR>\n"%s"\n\n!=\n\n"%s"\n' % (p1.stderr, p2.stderr))
 
             if not self.unattended:
-              self.set_up_command_for_user(jove_loop_args)
+              self.set_up_command_for_user(["ssh"] + jove_loop_args)
 
             return 1
 
