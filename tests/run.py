75950c5cc7d30d2146b314c1913bb41275d0e8c8
Aoto Tokizawa <aoto.tokizawa@gmail.com>
2024-05-28 05:28:32 +0000

    tests/run.py: use libtmux, no longer necessary to fuck around with fifos

--- tests/run.py
+++ tests/run.py
@@ -1,210 +1,26 @@
 #!/usr/bin/python3
 
-import tempfile
-import subprocess
-import sys
 import argparse
-import os
-import time
-import re
-import atexit
+import sys
 from pathlib import Path
+from tester import JoveTester
 
-parser = argparse.ArgumentParser(description='Run tests.')
-parser.add_argument('tests', metavar='T', type=str, nargs='+', help='tests to execute')
-parser.add_argument('-a', dest='arch', type=str, required=True, help='specify architecture')
-parser.add_argument('--preexisting', dest='preexisting', type=str, help='specify preexisting directory')
+def parse_arguments():
+  parser = argparse.ArgumentParser(description='Run tests.')
+  parser.add_argument('tests', metavar='T', type=str, nargs='+', help='tests to execute')
+  parser.add_argument('-a', dest='arch', type=str, required=True, help='specify architecture')
+  parser.add_argument('-u', '--unattended', action='store_true', help='Run in unattended mode')
 
-args = parser.parse_args()
+  return parser.parse_args()
 
-tests_dir = Path(__file__).parent.resolve()
+def main():
+  args = parse_arguments()
 
-jove_server_path = '%s/../llvm-project/build/bin/jove-%s' % (tests_dir, args.arch)
-assert(Path(jove_server_path).is_file())
+  tests_dir = str(Path(__file__).parent.resolve())
 
-jove_client_path = '%s/../llvm-project/%s_build/bin/jove-%s' % (tests_dir, args.arch, args.arch)
-assert(Path(jove_client_path).is_file())
+  tester = JoveTester(tests_dir, args.tests, args.arch, args.unattended)
+  return tester.run()
 
-jove_rt_path = '%s/../bin/%s/libjove_rt.st.so' % (tests_dir, args.arch)
-assert(Path(jove_rt_path).is_file())
-
-#td_path = "/root/deb-vm-" + args.arch
-#os.mkdir(td_path)
-#d = td_path
-
-td = tempfile.TemporaryDirectory()
-d = td.name
-
-arch2ports = dict()
-arch2ports["i386"]     = 10023
-arch2ports["x86_64"]   = 10024
-arch2ports["aarch64"]  = 10025
-arch2ports["mipsel"]   = 10026
-arch2ports["mips"]     = 10027
-arch2ports["mips64el"] = 10028
-
-guest_ssh_port = arch2ports[args.arch]
-jove_server_port = guest_ssh_port - 5000
-
-if args.preexisting is None:
-  bringup_path = '%s/../mk-deb-vm/bringup.sh' % str(tests_dir)
-
-  subprocess.run(['sudo', bringup_path, '-a', args.arch, '-s', 'bookworm', '-o', d, '-p', str(guest_ssh_port), '-f'], check=True)
-else:
-  d = args.preexisting
-
-ififo_path = '%s/x.in' % d
-ofifo_path = '%s/x.out' % d
-
-if os.path.exists(ififo_path):
-  os.remove(ififo_path)
-
-if os.path.exists(ofifo_path):
-  os.remove(ofifo_path)
-
-os.mkfifo(ififo_path)
-os.mkfifo(ofifo_path)
-
-cp = subprocess.Popen(['cat', '%s/x.out' % d], stdout=open('%s/stdout.txt' % d, 'wb', buffering=0), stdin=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-
-os.chdir(d)
-qp = subprocess.Popen(['%s/run.sh' % d, '-serial', 'pipe:x'], stdin=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
-
-def cleanup():
-  cp.kill()
-  qp.kill()
-
-atexit.register(cleanup)
-
-def serial_write(s, t=0.5):
-  if qp.poll() != None or cp.poll() != None:
-    return False
-
-  inf = open('%s/x.in' % d, 'wb', buffering=0)
-  inf.write(s.encode())
-  inf.close()
-
-  time.sleep(t)
-
-  return True
-
-ansi_escape = re.compile(r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])')
-def strip_ansi_escape_sequences(s):
-  return ansi_escape.sub('', s)
-
-def serial_tail(n=1):
-  completed_process = subprocess.run(['tail', '-n', str(n), '%s/stdout.txt' % d], capture_output=True, text=True)
-  return strip_ansi_escape_sequences(completed_process.stdout)
-
-def ssh_command(command, text=True):
-  return subprocess.run(['ssh', '-o', 'UserKnownHostsFile=/dev/null', '-o', 'StrictHostKeyChecking=no', '-o', 'LogLevel=quiet', '-p', str(guest_ssh_port), 'root@localhost'] + command, capture_output=True, text=text)
-
-def ssh(command):
-  return subprocess.run(['ssh', '-o', 'UserKnownHostsFile=/dev/null', '-o', 'StrictHostKeyChecking=no', '-o', 'LogLevel=quiet', '-p', str(guest_ssh_port), 'root@localhost'] + command)
-
-def scp(src, dst):
-  return subprocess.run(['scp', '-o', 'UserKnownHostsFile=/dev/null', '-o', 'StrictHostKeyChecking=no', '-o', 'LogLevel=quiet', '-P', str(guest_ssh_port), src, 'root@localhost:' + dst])
-
-def inputs_for_test(test):
-  inputs_path = '%s/inputs/%s.inputs' % (tests_dir, test)
-
-  assert(os.path.exists(inputs_path))
-  return eval(open(inputs_path, 'r').read())
-
-#
-# wait for system to boot up
-#
-print("waiting for system to boot...")
-
-time.sleep(0.5)
-
-while not serial_tail().strip().endswith("login:"):
-  assert(serial_write("\n"))
-
-print("system booted.")
-
-#
-# get IP of host seen by guest
-#
-iphost = ssh_command(['ip', 'route', 'show']).stdout.strip().split()[2]
-print("iphost: %s" % iphost)
-
-#
-# start jove server
-#
-jp = subprocess.Popen([jove_server_path, 'server', '-v', '--port=%d' % jove_server_port], stdin=subprocess.DEVNULL)
-
-#
-# prepare to run jove under emulation
-#
-scp(jove_client_path, '/usr/local/bin/jove')
-scp(jove_rt_path, '/lib/libjove_rt.so')
-
-def run_tests():
-  for test in args.tests:
-    test_inputs = inputs_for_test(test)
-    test_bin = '%s/bin/%s/%s' % (tests_dir, args.arch, test)
-
-    for variant in ["exe", "pic"]:
-      test_bin_path = '%s.%s' % (test_bin, variant);
-      test_bin_name = Path(test_bin_path).name
-
-      print("test %s" % test_bin_path)
-
-      assert(Path(test_bin_path).is_file())
-
-      scp(test_bin_path, '/tmp/')
-
-      test_guest_path = '/tmp/%s' % test_bin_name
-
-      ssh(["rm", "-f", "/root/.jv"]) # FIXME
-
-      ssh(["jove", "init", test_guest_path])
-      for input_args in test_inputs:
-        ssh(["jove", "bootstrap", test_guest_path] + input_args)
-
-      jove_loop_args = ["jove", "loop", "--mt=0", "--optimize", "--connect", "%s:%d" % (iphost, jove_server_port), test_guest_path]
-
-      for i in range(0, 2):
-        for input_args in test_inputs:
-          ssh(jove_loop_args + input_args)
-
-      for input_args in test_inputs:
-        p1 = ssh_command([test_guest_path] + input_args, text=True)
-        p2 = ssh_command(jove_loop_args + input_args, text=True)
-
-        if p2.returncode != 0 and p1.returncode == 0:
-          print("TESTS FAILURE_1 %s [%s]" % (test_bin_path, args.arch))
-          return 1
-
-        stdout_neq = p1.stdout != p2.stdout
-        stderr_neq = p1.stderr != p2.stderr
-        if stdout_neq or stderr_neq:
-          if stdout_neq:
-            print("TESTS FAILURE_STDOUT [%s] %s\n\n\"%s\"\n\n!=\n\n\"%s\"\n\n" % (args.arch, test_bin_path, p1.stdout, p2.stdout))
-          if stderr_neq:
-            print("TESTS FAILURE_STDERR [%s] %s\n\n\"%s\"\n\n!=\n\n\"%s\"\n\n" % (args.arch, test_bin_path, p1.stderr, p2.stderr))
-          return 1
-
-  return 0
-
-exit_code = run_tests()
-
-#
-# power off system
-#
-print("powering off system...")
-
-ssh(['systemctl', 'poweroff'])
-
-qp.communicate()
-cp.communicate()
-jp.kill()
-
-if os.path.exists(ififo_path):
-  os.remove(ififo_path)
-
-if os.path.exists(ofifo_path):
-  os.remove(ofifo_path)
-
-sys.exit(exit_code)
+if __name__ == "__main__":
+  rc = main()
+  sys.exit(rc)
---
+++ tests/tester.py
@@ -0,0 +1,277 @@
+from pathlib import Path
+import subprocess
+import tempfile
+import libtmux
+import time
+import os
+
+class JoveTester:
+  ARCH2PORT = {
+    'i386'    : 10023,
+    'x86_64'  : 10024,
+    'aarch64' : 10025,
+    'mipsel'  : 10026,
+    'mips'    : 10027,
+    'mips64el': 10028
+  }
+
+  def __init__(self, tests_dir, tests, arch, unattended=False):
+    self.tests_dir = tests_dir
+    self.tests = tests
+    self.arch = arch
+    self.unattended = unattended
+
+    assert arch in JoveTester.ARCH2PORT, "invalid arch"
+
+    self.guest_ssh_port = JoveTester.ARCH2PORT[self.arch]
+    self.jove_server_port = self.guest_ssh_port - 5000
+
+    self.find_things()
+
+    self.vm_dir = os.getenv("JOVE_VM_" + arch.upper())
+    if self.vm_dir is None:
+      self.td = tempfile.TemporaryDirectory()
+      self.vm_dir = self.td.name
+    else:
+      assert os.path.isdir(self.vm_dir), "VM path not directory"
+
+    self.tmux = libtmux.Server()
+
+  def find_things(self):
+    self.jove_server_path = '%s/../llvm-project/build/bin/jove-%s' % (self.tests_dir, self.arch)
+    assert Path(self.jove_server_path).is_file(), "missing host jove binary"
+
+    self.jove_client_path = '%s/../llvm-project/%s_build/bin/jove-%s' % (self.tests_dir, self.arch, self.arch)
+    assert Path(self.jove_client_path).is_file(), "missing guest jove binary"
+
+    self.jove_rt_path = '%s/../bin/%s/libjove_rt.st.so' % (self.tests_dir, self.arch)
+    assert Path(self.jove_rt_path).is_file(), "missing jove runtime"
+
+    self.bringup_path = '%s/../mk-deb-vm/bringup.sh' % self.tests_dir
+    assert Path(self.bringup_path).is_file(), "missing mk-deb-vm/bringup.sh"
+
+  def session_name(self):
+    return "jove_" + self.arch
+
+  def find_or_create_tmux_session(self):
+    self.sess = None
+    self.wins = [None, None, None]
+
+    create_qemu = False
+    create_serv = False
+    create_ssh = False
+
+    for sess in self.tmux.sessions:
+      if sess.name == self.session_name():
+        self.sess = sess
+        break
+
+    if self.sess is None:
+      print('creating tmux session "%s"' % self.session_name())
+
+      self.sess = self.tmux.new_session(session_name=self.session_name(), window_name="qemu")
+      create_qemu = True
+
+      assert self.sess.windows[0].name == "qemu"
+
+    for win in self.sess.windows:
+      if win.name == "qemu":
+        self.wins[0] = win
+      elif win.name == "server":
+        self.wins[1] = win
+      elif win.name == "ssh":
+        self.wins[2] = win
+
+    if self.wins[0] is None:
+      create_qemu = True
+      self.wins[0] = self.sess.new_window(window_name="qemu")
+
+    if self.wins[1] is None:
+      create_serv = True
+      self.wins[1] = self.sess.new_window(window_name="server")
+
+    if self.wins[2] is None:
+      create_ssh = True
+      self.wins[2] = self.sess.new_window(window_name="ssh")
+
+    return (create_qemu, create_serv, create_ssh)
+
+  def vm_run_path(self):
+    return self.vm_dir + "/run.sh"
+
+  def exists_vm(self):
+    run_path = self.vm_run_path()
+    return os.path.exists(run_path) and Path(run_path).is_file()
+
+  def create_vm(self):
+    print("creating VM...")
+
+    subprocess.run(['sudo', self.bringup_path, '-a', self.arch, '-s', 'bookworm', '-o', self.vm_dir, '-p', str(self.guest_ssh_port)], check=True)
+
+  def qemu_pane(self):
+    return self.wins[0].attached_pane
+
+  def server_pane(self):
+    return self.wins[1].attached_pane
+
+  def ssh_pane(self):
+    return self.wins[2].attached_pane
+
+  def start_vm(self):
+    print("starting VM...")
+
+    qp = self.qemu_pane()
+
+    qp.send_keys('clear')
+    qp.send_keys('cd "%s"' % self.vm_dir)
+    qp.send_keys('./run.sh')
+
+  def start_server(self):
+    print("starting jove server...")
+
+    command = [self.jove_server_path, 'server', '-v', '--port=%d' % self.jove_server_port]
+
+    pane = self.server_pane()
+    pane.send_keys(" ".join(command))
+
+  def is_vm_ready(self):
+    qp = self.qemu_pane()
+
+    return any("login:" in row for row in qp.capture_pane())
+
+  def wait_for_vm_ready(self, t=1.5):
+    qp = self.qemu_pane()
+
+    while not self.is_vm_ready():
+      print("waiting for VM...")
+
+      time.sleep(t)
+      qp.send_keys('')
+
+    print("VM ready.")
+
+  def set_up_command_for_user(self, command):
+    self.sess.select_window("ssh")
+
+    pane = self.ssh_pane()
+    pane.send_keys("C-c", literal=False, enter=False)
+    pane.send_keys(" ".join(command), enter=False)
+
+  def ssh_command(self, command, text=True):
+    return subprocess.run(['ssh', '-o', 'UserKnownHostsFile=/dev/null', '-o', 'StrictHostKeyChecking=no', '-o', 'LogLevel=quiet', '-p', str(self.guest_ssh_port), 'root@localhost'] + command, capture_output=True, text=text)
+
+  def ssh(self, command):
+    return subprocess.run(['ssh', '-o', 'UserKnownHostsFile=/dev/null', '-o', 'StrictHostKeyChecking=no', '-o', 'LogLevel=quiet', '-p', str(self.guest_ssh_port), 'root@localhost'] + command)
+
+  def scp(self, src, dst):
+    return subprocess.run(['scp', '-o', 'UserKnownHostsFile=/dev/null', '-o', 'StrictHostKeyChecking=no', '-o', 'LogLevel=quiet', '-P', str(self.guest_ssh_port), src, 'root@localhost:' + dst])
+
+  def prepare_run_tests(self):
+    print("preparing to run tests...")
+
+    self.scp(self.jove_client_path, '/usr/local/bin/jove')
+    self.scp(self.jove_rt_path, '/lib/libjove_rt.so')
+
+  def inputs_for_test(self, test):
+    inputs_path = '%s/inputs/%s.inputs' % (self.tests_dir, test)
+
+    assert os.path.exists(inputs_path), "no input for %s" % test
+
+    return eval(open(inputs_path, 'r').read())
+
+  def run_tests(self):
+    print("running %d tests..." % len(self.tests))
+
+    for test in self.tests:
+      test_inputs = self.inputs_for_test(test)
+      test_bin = '%s/bin/%s/%s' % (self.tests_dir, self.arch, test)
+
+      for variant in ["exe", "pic"]:
+        test_bin_path = '%s.%s' % (test_bin, variant);
+        test_bin_name = Path(test_bin_path).name
+
+        print("test %s" % test_bin_path)
+
+        assert(Path(test_bin_path).is_file())
+
+        self.scp(test_bin_path, '/tmp/')
+
+        test_guest_path = '/tmp/%s' % test_bin_name
+
+        self.ssh(["rm", "-f", "/root/.jv"]) # FIXME
+
+        self.ssh(["jove", "init", test_guest_path])
+        for input_args in test_inputs:
+          self.ssh(["jove", "bootstrap", test_guest_path] + input_args)
+
+        jove_loop_args = ["jove", "loop", "--mt=0", "--optimize", "--connect", "%s:%d" % (self.iphost, self.jove_server_port), test_guest_path]
+
+        for i in range(0, 2):
+          for input_args in test_inputs:
+            self.ssh(jove_loop_args + input_args)
+
+        for input_args in test_inputs:
+          p1 = self.ssh_command([test_guest_path] + input_args, text=True)
+          p2 = self.ssh_command(jove_loop_args + input_args, text=True)
+
+          return_neq = p1.returncode != p2.returncode
+          stdout_neq = p1.stdout != p2.stdout
+          stderr_neq = p1.stderr != p2.stderr
+
+          failed = return_neq or stdout_neq or stderr_neq
+          if failed:
+            print("/////////\n///////// TEST FAILURE %s <%s>\n/////////" % (test_bin_path, self.arch))
+            if return_neq:
+              print('%d != %d' % (p1.returncode, p2.returncode))
+            if stdout_neq:
+              print('<STDOUT>\n"%s"\n\n!=\n\n"%s"\n' % (p1.stdout, p2.stdout))
+            if stderr_neq:
+              print('<STDERR>\n"%s"\n\n!=\n\n"%s"\n' % (p1.stderr, p2.stderr))
+
+            if not self.unattended:
+              self.set_up_command_for_user(jove_loop_args)
+
+            return 1
+
+    return 0
+
+  def run(self):
+    create_qemu, create_serv, create_ssh = self.find_or_create_tmux_session()
+
+    if create_qemu:
+      if not self.exists_vm():
+        self.create_vm()
+      self.start_vm()
+
+    if not self.is_vm_ready():
+      self.wait_for_vm_ready()
+
+    #
+    # get IP of host seen by guest
+    #
+    self.iphost = self.ssh_command(['ip', 'route', 'show']).stdout.strip().split()[2]
+    print("iphost: %s" % self.iphost)
+
+    #
+    # start jove server
+    #
+    if create_serv:
+      self.start_server()
+
+    self.prepare_run_tests()
+
+    #
+    # run tests
+    #
+    rc = self.run_tests()
+
+    if self.unattended:
+      if create_qemu:
+        ssh(['systemctl', 'poweroff'])
+        self.sess.kill_window("qemu")
+      if create_serv:
+        self.sess.kill_window("server")
+      if create_ssh:
+        self.sess.kill_window("ssh")
+
+    return rc
