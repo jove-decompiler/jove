fc44d99d3c51a0e9111e41a4db745179cf639bb6
Aoto Tokizawa <aoto.tokizawa@gmail.com>
2024-06-01 16:25:56 +0000

    ci_build_carbon_copy.sh: fix "No CMAKE_CXX_COMPILER could be found."

--- scripts/ci_build_carbon_copy.sh
+++ scripts/ci_build_carbon_copy.sh
@@ -7,7 +7,7 @@ pushd .
 cd /jove/carbon-copy
 mkdir build && cd build
 
-cmake -G Ninja -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=/usr/local ..
+CXX=clang++-16 cmake -G Ninja -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=/usr/local ..
 
 ninja install
 
