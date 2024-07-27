efd0434c4ae28bcc4ffc028585f237269a94d31f
Aoto Tokizawa <aoto.tokizawa@gmail.com>
2024-07-26 23:19:00 -0400

    scripts/build/llvm/build_x86_64.debug.san.sh: make note of the "Endless AddressSanitizer:DEADLYSIGNAL loop", turn PIC off, besides that use clang-tblgen already made, oh and CMAKE_BUILD_TYPE=RelWithDebInfo

--- scripts/build/llvm/build_x86_64.debug.san.sh
+++ scripts/build/llvm/build_x86_64.debug.san.sh
@@ -3,16 +3,27 @@ set -e
 set -o pipefail
 set -x
 
+#
+# "Endless AddressSanitizer:DEADLYSIGNAL loop"
+# https://reviews.llvm.org/D148280
+#
+# $ sudo sysctl vm.mmap_rnd_bits=28
+#
+# and down below we are turning LLVM_ENABLE_PIC=OFF with the hope that it will
+# improve the situation
+#
+
 if [ ! -f build.ninja ]; then
 
 cmake -G Ninja \
-      -D CMAKE_BUILD_TYPE=Debug \
+      -D CMAKE_BUILD_TYPE=RelWithDebInfo \
       -D CMAKE_C_COMPILER=$(which clang-16) \
       -D CMAKE_CXX_COMPILER=$(which clang++-16) \
       -D "LLVM_TARGETS_TO_BUILD=Mips;X86;AArch64" \
       -D "JOVE_TARGETS_TO_BUILD=i386;x86_64;mipsel;mips64el;aarch64" \
       -D JOVE_HAVE_MEMFD=ON \
       -D "LLVM_TABLEGEN=$(pwd)/../build/bin/llvm-tblgen" \
+      -D "CLANG_TABLEGEN=$(pwd)/../build/bin/clang-tblgen" \
       -D LLVM_BUILD_TESTS=OFF \
       -D LLVM_INCLUDE_TESTS=OFF \
       -D LLVM_ENABLE_BINDINGS=OFF \
@@ -27,7 +38,7 @@ cmake -G Ninja \
       -D LLVM_ENABLE_EH=ON \
       -D LLVM_BUILD_DOCS=OFF \
       -D LLVM_BINUTILS_INCDIR=/usr/include \
-      -D LLVM_ENABLE_PIC=ON \
+      -D LLVM_ENABLE_PIC=OFF \
       -D LLVM_ENABLE_Z3_SOLVER=OFF \
       -D JOVE_USE_SYSTEM_TBB=ON \
       -D "LLVM_USE_SANITIZER=Address;Undefined" \
@@ -37,4 +48,6 @@ cmake -G Ninja \
 
 fi
 
-ninja include/llvm/IR/Attributes.inc && ninja bin/jove-{x86_64,i386,aarch64,mipsel,mips64el}
+#     -D LLVM_ENABLE_LIBCXX=ON
+
+ninja include/llvm/IR/Attributes.inc && ninja bin/{llvm-tblgen,llvm-dis,llvm-dlltool,llvm-cbe,opt,llc,clang,clang-tblgen,lld,jove-x86_64,jove-i386,jove-aarch64,jove-mipsel,jove-mips64el}
