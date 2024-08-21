f3ba7c4abf58dc42496ec971ddb5661abc045b5b
Aoto Tokizawa <aoto.tokizawa@gmail.com>
2024-08-21 01:19:59 -0400

    introduce scoped_mmap

---
+++ core/mmap.cpp
@@ -0,0 +1,25 @@
+#include "mmap.h"
+#include <sys/mman.h>
+#include <stdexcept>
+#include <cstring>
+
+namespace jove {
+
+scoped_mmap::scoped_mmap(void *addr, size_t len, int prot, int flags, int fd,
+                         uint64_t off)
+    : len(len), ptr(::mmap(addr, len, prot, flags, fd, off)) {}
+
+scoped_mmap::~scoped_mmap() noexcept(false) {
+  if (failed())
+    return;
+
+  if (::munmap(ptr, len) < 0)
+    throw std::runtime_error(std::string("scoped_mmap: munmap failed: ") +
+                             strerror(errno));
+}
+
+bool scoped_mmap::failed(void) const {
+  return ptr == MAP_FAILED;
+}
+
+}
---
+++ core/mmap.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <cstdint>
+#include <cstddef>
+
+namespace jove {
+
+struct scoped_mmap {
+  const size_t len;
+  void *const ptr;
+
+  scoped_mmap(void *addr, size_t len, int prot, int flags, int fd, uint64_t off);
+  ~scoped_mmap() noexcept(false); /* throws if failed to unmap memory */
+
+  bool failed(void) const; /* is ptr equal to MAP_FAILED */
+
+  operator bool(void) const { return !failed(); }
+};
+
+}
