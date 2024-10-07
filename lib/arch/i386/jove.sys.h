1bcdf986fc2d99cbce74f78a144ef53e873b692d
Aoto Tokizawa <aoto.tokizawa@gmail.com>
2024-10-07 00:31:58 -0400

    syscalls.inc.h (i386): rename mmap_pgoff to mmap

--- lib/arch/i386/jove.sys.h
+++ lib/arch/i386/jove.sys.h
@@ -11,13 +11,12 @@
 #include <sys/mman.h>
 
 static uintptr_t _mmap_rw_anonymous_private_memory(size_t len) {
-  return _jove_sys_mmap_pgoff(0x0, len, PROT_READ | PROT_WRITE,
-                              MAP_PRIVATE | MAP_ANONYMOUS, -1L, 0);
+  return _jove_sys_mmap(0x0, len, PROT_READ | PROT_WRITE,
+                        MAP_PRIVATE | MAP_ANONYMOUS, -1L, 0);
 }
 
 static uintptr_t _mmap_rw_shared_file(int fd, size_t len) {
-  return _jove_sys_mmap_pgoff(0x0, len, PROT_READ | PROT_WRITE,
-                              MAP_SHARED, fd, 0);
+  return _jove_sys_mmap(0x0, len, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
 }
 
 
