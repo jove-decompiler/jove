56b9d407fe1e4ed2860ac9331cccf82d5779a930
Aoto Tokizawa <aoto.tokizawa@gmail.com>
2024-12-24 00:20:59 -0800

    ci: set LLVM_SYMBOLIZER_PATH

--- scripts/ci_check.sh
+++ scripts/ci_check.sh
@@ -3,5 +3,6 @@
 ssh-keygen -q -t rsa -b 4096 -N '' -f ~/.ssh/id_rsa <<<y >/dev/null 2>&1
 
 export JOVE_TEST_UNATTENDED=1
+export LLVM_SYMBOLIZER_PATH=$(which llvm-symbolizer-16)
 
 make -C jove check -j$(nproc)
