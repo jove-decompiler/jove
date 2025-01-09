cad328a3e79af39e9688f78aa66a287bf5d6894c
Aoto Tokizawa <aoto.tokizawa@gmail.com>
2025-01-08 19:42:28 -0800

    ci_check.sh: set LANG to see if this fixes libtmux issues

--- scripts/ci_check.sh
+++ scripts/ci_check.sh
@@ -5,4 +5,6 @@ ssh-keygen -q -t rsa -b 4096 -N '' -f ~/.ssh/id_rsa <<<y >/dev/null 2>&1
 export JOVE_TEST_UNATTENDED=1
 export LLVM_SYMBOLIZER_PATH=$(which llvm-symbolizer-16)
 
+export LANG=en_US.UTF-8
+
 make -C jove check -j$(nproc)
