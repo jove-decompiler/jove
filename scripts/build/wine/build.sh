45a45033ea24c8b29f586a9c9f6f36b815524ef0
Aoto Tokizawa <aoto.tokizawa@gmail.com>
2025-01-14 07:37:57 -0800

    scripts/build: for wine

---
+++ scripts/build/wine/build.sh
@@ -0,0 +1,1 @@
+PKG_CONFIG_PATH=/usr/lib/i386-linux-gnu/pkgconfig ../configure --prefix=/usr --with-x --with-wayland --with-gstreamer --libdir=/usr/lib32 --with-vulkan --with-wine64=$(pwd)/../build64
