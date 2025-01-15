7cd49694c5eff1fd78af6e01a3f45050bb3ff6bf
Aoto Tokizawa <aoto.tokizawa@gmail.com>
2025-01-14 22:04:13 -0800

    scripts/build: wine: actually make

--- scripts/build/wine/build.sh
+++ scripts/build/wine/build.sh
@@ -5,4 +5,4 @@ set -x
 
 PKG_CONFIG_PATH=/usr/lib/i386-linux-gnu/pkgconfig ../configure --prefix=/usr --with-x --with-wayland --with-gstreamer --libdir=/usr/lib32 --with-vulkan --with-wine64=$(pwd)/../build64
 
-#make -j$(nproc)
+make -j$(nproc)
--- scripts/build/wine/build64.sh
+++ scripts/build/wine/build64.sh
@@ -6,4 +6,4 @@ set -x
 # gstreamer not enabled because we cannot have development files for 32 and 64
 ../configure --prefix=/usr --libdir=/usr/lib --with-x --with-wayland --with-vulkan --enable-win64
 
-#make -j$(nproc)
+make -j$(nproc)
