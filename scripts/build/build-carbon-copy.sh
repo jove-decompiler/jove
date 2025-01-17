d1bb73b5050df0b55f3587295f7d1721425854cf
Aoto Tokizawa <aoto.tokizawa@gmail.com>
2025-01-17 00:45:55 -0800

    scripts: Update

--- scripts/build/build-carbon-copy.sh
+++ scripts/build/build-carbon-copy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -x
 
-cd ~/jove/carbon-copy
+cd /jove/carbon-copy
 mkdir build && cd build
 
 CC=clang-19 CXX=clang++-19 cmake -G Ninja -D CMAKE_BUILD_TYPE=Release -S $(pwd)/.. -B $(pwd)
