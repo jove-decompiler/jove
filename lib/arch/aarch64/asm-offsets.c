61d35971bec83944a952fc326a5a76c0ccdb6b79
Aoto Tokizawa <aoto.tokizawa@gmail.com>
2025-03-06 14:57:05 -0800

    asm-offsets (aarch64): fix typo

--- lib/arch/aarch64/asm-offsets.c
+++ lib/arch/aarch64/asm-offsets.c
@@ -3,7 +3,7 @@
 int main(void) {
   DEFINE(ASMOFF_ENV_cp15_tpidr_el_0_, offsetof(CPUARMState, cp15.tpidr_el[0]));
   DEFINE(ASMOFF_ENV_vfp_zregs_0_, offsetof(CPUARMState, vfp.zregs[0]));
-  DEFINE(ASMOFF_ENV_vfp_zregs_32_, offsetof(CPUARMState, cp15.tpidr_el[0]));
+  DEFINE(ASMOFF_ENV_vfp_zregs_32_, offsetof(CPUARMState, vfp.zregs[32]));
   DEFINE(ASMOFF_ENV_btype, offsetof(CPUARMState, btype));
 
   return 0;
