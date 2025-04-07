fcd2955f3249e258109e229b3fc3078e7556a017
Aoto Tokizawa <aoto.tokizawa@gmail.com>
2025-04-06 22:48:46 -0700

    style (split up jove.h)

---
+++ include/jove/algo.h
@@ -0,0 +1,27 @@
+#include <algorithm>
+
+namespace jove {
+
+template <typename _ExecutionPolicy, typename Iter, typename Pred, typename Proc>
+constexpr
+void for_each_if(_ExecutionPolicy &&__exec, Iter first, Iter last, Pred pred, Proc proc) {
+  std::for_each(std::forward<_ExecutionPolicy>(__exec), first, last,
+                [pred, proc](auto &elem) {
+                  if (pred(elem)) {
+                    proc(elem);
+                  }
+                });
+}
+
+template <typename Iter, typename Pred, typename Proc>
+constexpr
+void for_each_if(Iter first, Iter last, Pred pred, Proc proc) {
+  std::for_each(first, last,
+                [pred, proc](auto &elem) {
+                  if (pred(elem)) {
+                    proc(elem);
+                  }
+                });
+}
+
+}
