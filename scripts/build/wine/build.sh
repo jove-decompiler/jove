57840e37353a183a1cfaf3836c4f4f59cb4cbc2e
Aoto Tokizawa <aoto.tokizawa@gmail.com>
2025-04-15 14:45:33 -0700

    scripts: build: wine: --libdir=/usr/lib for 32 and 64 bit

--- scripts/build/wine/build.sh
+++ scripts/build/wine/build.sh
@@ -5,7 +5,7 @@ set -x
 
 if [ ! -f Makefile ]; then
 
-PKG_CONFIG_PATH=/usr/lib/i386-linux-gnu/pkgconfig ../configure --prefix=/usr --with-x --with-wayland --with-gstreamer --libdir=/usr/lib32 --with-vulkan --with-wine64=$(pwd)/../build64
+PKG_CONFIG_PATH=/usr/lib/i386-linux-gnu/pkgconfig ../configure --prefix=/usr --with-x --with-wayland --with-gstreamer --libdir=/usr/lib --with-vulkan --with-wine64=$(pwd)/../build64
 
 fi
 
