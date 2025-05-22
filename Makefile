90c046730b80c6268b7d49bb8babf4d460535a37
Aoto Tokizawa <aoto.tokizawa@gmail.com>
2025-05-22 16:36:14 -0700

    we need to treat tcgconstants.h like qemu.tcg.copy.h

--- Makefile
+++ Makefile
@@ -129,7 +129,8 @@ utilities: $(UTILBINS) $(UTILINCS)
 asm-offsets: $(foreach t,$(ALL_TARGETS),$(BINDIR)/$(t)/asm-offsets.h)
 
 .PHONY: tcg-constants
-tcg-constants: $(foreach t,$(ALL_TARGETS),$(BINDIR)/$(t)/tcgconstants.h)
+tcg-constants: $(foreach t,$(ALL_TARGETS),$(BINDIR)/$(t)/tcgconstants.h) \
+               $(foreach t,$(ALL_TARGETS),$(BINDIR)/$(HOST_TARGET)/tcgconstants.$(t).h)
 
 .PHONY: all-helpers-mk
 all-helpers-mk: $(foreach t,$(ALL_TARGETS),all-helpers-$(t)-mk)
@@ -365,6 +366,9 @@ $(BINDIR)/$(HOST_TARGET)/qemu.tcg.copy.$(1).h:
 	$(CARBON_EXTRACT) --src $(QEMU_DIR) --bin $(call qemu_carbon_host_build_dir,$(1)) -n --flatten jove_tcg >> $$@
 
 $(BINDIR)/$(1)/tcgconstants.h: | $(BINDIR)/$(1)/qemu-starter
+	env JOVE_PRINT_CONSTANTS=1 $(call qemu_carbon_build_dir,$(1))/qemu-$(1) $(BINDIR)/$(1)/qemu-starter > $$@
+
+$(BINDIR)/$(HOST_TARGET)/tcgconstants.$(1).h: | $(BINDIR)/$(1)/qemu-starter
 	env JOVE_PRINT_CONSTANTS=1 $(call qemu_carbon_host_build_dir,$(1))/qemu-$(1) $(BINDIR)/$(1)/qemu-starter > $$@
 
 .PHONY: all-helpers-$(1)-mk
--- include/jove/tcg.h
+++ include/jove/tcg.h
@@ -1,6 +1,48 @@
 #pragma once
 
-#include "tcgconstants.h"
+#if defined(__x86_64__)
+#if defined(TARGET_X86_64)
+#include "../bin/x86_64/tcgconstants.h"
+#elif defined(TARGET_I386)
+#include "../bin/x86_64/tcgconstants.i386.h"
+#elif defined(TARGET_AARCH64)
+#include "../bin/x86_64/tcgconstants.aarch64.h"
+#elif defined(TARGET_MIPS64)
+#include "../bin/x86_64/tcgconstants.mips64el.h"
+#elif defined(TARGET_MIPS32) && defined(TARGET_MIPSEL)
+#include "../bin/x86_64/tcgconstants.mipsel.h"
+#elif defined(TARGET_MIPS32) && defined(TARGET_MIPS)
+#include "../bin/x86_64/tcgconstants.mips.h"
+#else
+#error
+#endif
+#elif defined(__i386__)
+#ifdef TARGET_I386
+#include "../bin/i386/tcgconstants.h"
+#else
+#error
+#endif
+#elif defined(__aarch64__)
+#ifdef TARGET_AARCH64
+#include "../bin/aarch64/tcgconstants.h"
+#else
+#error
+#endif
+#elif defined(__mips64) && __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+#ifdef TARGET_MIPS64
+#include "../bin/mips64el/tcgconstants.h"
+#else
+#error
+#endif
+#elif defined(__mips__) && __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+#ifdef TARGET_MIPSEL
+#include "../bin/mipsel/tcgconstants.h"
+#else
+#error
+#endif
+#else
+#error
+#endif
 
 #ifdef __cplusplus
 #include <vector>
