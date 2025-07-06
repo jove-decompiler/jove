0120cff9a52ba9b8df3fdcf8a56f08b458dd30d9
Aoto Tokizawa <aoto.tokizawa@gmail.com>
2025-07-06 11:12:48 -0700

    scripts/ci_install.sh: fix it for the poor bastards who are using the docker image

--- scripts/ci_install.sh
+++ scripts/ci_install.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-ln -s /home/aeden/jove/llvm-project/build/llvm/bin/jove-x86_64 /usr/local/bin/
-ln -s /home/aeden/jove/llvm-project/build/llvm/bin/jove-i386 /usr/local/bin/
-ln -s /home/aeden/jove/llvm-project/build/llvm/bin/jove-aarch64 /usr/local/bin/
-ln -s /home/aeden/jove/llvm-project/build/llvm/bin/jove-mipsel /usr/local/bin/
-ln -s /home/aeden/jove/llvm-project/build/llvm/bin/jove-mips64el /usr/local/bin/
+SCRIPT_PATH="${BASH_SOURCE[0]}"
+SCRIPT_DIR="${SCRIPT_PATH%/*}"
+
+ln -sf ${SCRIPT_DIR}/../llvm-project/build/llvm/bin/jove-x86_64   /usr/local/bin/
+ln -sf ${SCRIPT_DIR}/../llvm-project/build/llvm/bin/jove-i386     /usr/local/bin/
+ln -sf ${SCRIPT_DIR}/../llvm-project/build/llvm/bin/jove-aarch64  /usr/local/bin/
+ln -sf ${SCRIPT_DIR}/../llvm-project/build/llvm/bin/jove-mipsel   /usr/local/bin/
+ln -sf ${SCRIPT_DIR}/../llvm-project/build/llvm/bin/jove-mips64el /usr/local/bin/
