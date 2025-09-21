fdc54db11eb631c418352ed8d250e10b437fd050
Aoto Tokizawa <aoto.tokizawa@gmail.com>
2025-09-21 02:21:08 -0700

    B: Create: MT-Safe

--- core/B.cpp
+++ core/B.cpp
@@ -1,6 +1,8 @@
 #include "B.h"
 #include "triple.h"
+
 #include <llvm/ADT/StringRef.h>
+#include <llvm/IR/LLVMContext.h>
 
 namespace jove {
 namespace B {
@@ -31,24 +33,48 @@ getObjectFormatTypeName(llvm::Triple::ObjectFormatType Kind) {
 }
 
 std::unique_ptr<llvm::object::Binary> Create(llvm::StringRef Data) {
-  llvm::Expected<std::unique_ptr<llvm::object::Binary>> BinOrErr =
-      llvm::object::createBinary(llvm::MemoryBufferRef(Data, ""));
+  static std::conditional_t<AreWeMT, std::mutex, std::monostate> ContextMtx;
+  static std::unique_ptr<llvm::LLVMContext> Context;
 
-  if (!BinOrErr)
-    throw std::runtime_error("failed to create binary: " +
-                             llvm::toString(BinOrErr.takeError()));
+  std::unique_ptr<llvm::object::Binary> TheBin;
+  {
+    std::conditional_t<AreWeMT, std::unique_lock<std::mutex>, __do_nothing_t>
+        lck(ContextMtx);
 
-  std::unique_ptr<llvm::object::Binary> &Bin = BinOrErr.get();
-#if 0
-  if (!llvm::isa<ELFO>(Bin.get()) &&
-      (!llvm::isa<COFFO>(Bin.get()) ||
-       llvm::cast<COFFO>(Bin.get())->getBytesInAddress() != sizeof(taddr_t)))
-    throw std::runtime_error(
-        "unexpected binary type (" + std::to_string(Bin->getType()) + ") [" +
-        getObjectFormatTypeName(Bin->getTripleObjectFormat()).str());
-#endif
+    llvm::LLVMContext *pContext = Context.get();
+    if (!pContext) {
+      auto NewContext = std::make_unique<llvm::LLVMContext>();
+      pContext = NewContext.get();
+      assert(pContext);
+      Context = std::move(NewContext);
+    }
 
-  return std::move(*BinOrErr);
+    auto BinOrErr = llvm::object::createBinary(llvm::MemoryBufferRef(Data, ""),
+                                               pContext, false);
+
+    if (!BinOrErr)
+      throw std::runtime_error("failed to create binary: " +
+                               llvm::toString(BinOrErr.takeError()));
+    TheBin = std::move(*BinOrErr);
+  }
+
+  llvm::object::Binary &Bin = *TheBin;
+
+  bool Suitable = is_elf(Bin) || is_coff(Bin);
+  if (!Suitable) {
+    std::string Desc;
+
+    if (auto *Obj = llvm::dyn_cast<llvm::object::ObjectFile>(&Bin)) {
+      llvm::Triple TT = Obj->makeTriple();
+      Desc = TT.str();
+    } else {
+      Desc = std::to_string(Bin.getType());
+    }
+
+    throw std::runtime_error("unexpected binary type (" + Desc + ")");
+  }
+
+  return std::move(TheBin);
 }
 
 }
