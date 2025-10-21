17bffe9610c64701d890d5ea62ec8fb523ed2b26
Aoto Tokizawa <aoto.tokizawa@gmail.com>
2025-10-21 11:47:42 -0700

    scripts/build/llknife: -fuse-ld=lld

--- scripts/build/llknife/build16.sh
+++ scripts/build/llknife/build16.sh
@@ -7,7 +7,7 @@ llknife_build_scripts_path=$(cd "$(dirname -- "$0")"; pwd)
 
 if [ ! -f build.ninja ]; then
 
-CC=clang-19 CXX=clang++-19 cmake -G Ninja -D CMAKE_BUILD_TYPE=RelWithDebInfo -D LLVM_ENABLE_ASSERTIONS=ON -D LLVM_DIR=$llknife_build_scripts_path/../../../llvm-project/build/llvm/lib/cmake/llvm -S $(pwd)/.. -B $(pwd)
+CC=clang-19 CXX=clang++-19 LDFLAGS="-fuse-ld=lld" cmake -G Ninja -D CMAKE_BUILD_TYPE=RelWithDebInfo -D LLVM_ENABLE_ASSERTIONS=ON -D LLVM_DIR=$llknife_build_scripts_path/../../../llvm-project/build/llvm/lib/cmake/llvm -S $(pwd)/.. -B $(pwd)
 
 fi
 
--- scripts/build/llknife/build19.sh
+++ scripts/build/llknife/build19.sh
@@ -5,7 +5,7 @@ set -x
 
 if [ ! -f build.ninja ]; then
 
-CC=clang-19 CXX=clang++-19 cmake -G Ninja -D CMAKE_BUILD_TYPE=RelWithDebInfo -D LLVM_ENABLE_ASSERTIONS=ON -S $(pwd)/.. -B $(pwd)
+CC=clang-19 CXX=clang++-19 LDFLAGS="-fuse-ld=lld" cmake -G Ninja -D CMAKE_BUILD_TYPE=RelWithDebInfo -D LLVM_ENABLE_ASSERTIONS=ON -S $(pwd)/.. -B $(pwd)
 
 fi
 
