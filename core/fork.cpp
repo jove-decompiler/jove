4b489bca83f2a760464bed7ce05c9710776859ad
Aoto Tokizawa <aoto.tokizawa@gmail.com>
2025-10-22 07:24:04 -0700

    long_fork: support older kernels without pidfd_open(2)

--- core/fork.cpp
+++ core/fork.cpp
@@ -13,26 +13,27 @@ namespace jove {
 
 pid_t long_fork(std::function<int(void)> f) {
   scoped_fd our_pfd(pidfd_open(::getpid(), 0));
-
   const pid_t child = jove::fork();
   if (!child) {
     (void)::prctl(PR_SET_PDEATHSIG, SIGTERM);
 
-    const int poll_ret = ({
-      struct pollfd pfd = {.fd = our_pfd.get(), .events = POLLIN};
-      sys::retry_eintr(::poll, &pfd, 1, 0);
-    });
+    if (our_pfd) {
+      const int poll_ret = ({
+        struct pollfd pfd = {.fd = our_pfd.get(), .events = POLLIN};
+        sys::retry_eintr(::poll, &pfd, 1, 0);
+      });
 
-    aassert(poll_ret >= 0);
+      aassert(poll_ret >= 0);
 
-    our_pfd.close();
-    if (poll_ret != 0) {
-      //
-      // parent is already gone.
-      //
-      for (;;)
-        _exit(0);
-      __builtin_unreachable();
+      our_pfd.close();
+      if (poll_ret != 0) {
+        //
+        // parent is already gone.
+        //
+        for (;;)
+          _exit(0);
+        __builtin_unreachable();
+      }
     }
 
     int rc = 1;
