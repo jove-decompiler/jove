22d1176b1ac131892171edb9da41152864810643
Aoto Tokizawa <aoto.tokizawa@gmail.com>
2025-10-23 20:30:18 -0700

    scoped_mmap: this may as well just be header-only

--- core/mmap.h
+++ core/mmap.h
@@ -1,5 +1,5 @@
 #pragma once
-#include "jove/macros.h"
+#include "assert.h"
 
 #include <cstdint>
 #include <cstddef>
@@ -9,23 +9,25 @@
 namespace jove {
 
 struct scoped_mmap {
-  const size_t len = 0;
+  const size_t len = ~size_t(0);
   void *const ptr = nullptr;
 
   scoped_mmap() = delete;
   scoped_mmap(const scoped_mmap &) = delete;
   scoped_mmap &operator=(const scoped_mmap &) = delete;
+  scoped_mmap(scoped_mmap &&) = delete;
+  scoped_mmap &operator=(scoped_mmap &&) = delete;
 
-  explicit scoped_mmap(void *addr, size_t len, int prot, int flags, int fd, uint64_t off);
-  ~scoped_mmap() noexcept(false); /* throws if failed to unmap memory */
+  explicit scoped_mmap(void *addr, size_t len, int prot, int flags, int fd,
+                       uint64_t off) noexcept
+      : len(len), ptr(::mmap(addr, len, prot, flags, fd, off)) {}
 
-  [[clang::always_inline]] bool failed(void) const {
-    return !ptr || unlikely(ptr == MAP_FAILED);
+  ~scoped_mmap() noexcept(false) { /* throws if failed to unmap memory */
+    if (*this)
+      aassert(::munmap(ptr, len) == 0);
   }
 
-  [[clang::always_inline]] explicit operator bool(void) const {
-    return !failed();
-  }
+  explicit operator bool(void) const { return ptr && ptr != MAP_FAILED; }
 };
 
 }
