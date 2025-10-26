1b73c3d12fcac4136b220ce25a5721374d5c0d38
Aoto Tokizawa <aoto.tokizawa@gmail.com>
2025-10-25 20:24:52 -0700

    style (comment)

--- core/tool.cpp
+++ core/tool.cpp
@@ -246,7 +246,7 @@ int main(int argc, char **argv) {
 
   //
   // if we get here, an exception occurred. some thing may be in an "undefined"
-  // state. At this point, behave as though `execve("/usr/bin/true")` occurred.
+  // state. At this point, behave as though `execve("/usr/bin/false")` occurred.
   //
   llvm::errs() << message;
   llvm::errs().flush();
