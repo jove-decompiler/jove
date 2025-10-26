375173aa09eae81c0def73fbbedd15ce02f922a0
Aoto Tokizawa <aoto.tokizawa@gmail.com>
2025-10-26 02:58:31 -0700

    scripts/ci/install.sh: fix typo

--- scripts/ci/install.sh
+++ scripts/ci/install.sh
@@ -3,8 +3,8 @@
 SCRIPT_PATH="${BASH_SOURCE[0]}"
 SCRIPT_DIR="${SCRIPT_PATH%/*}"
 
-ln -sf ${SCRIPT_DIR}/../llvm-project/build/llvm/bin/jove-x86_64   /usr/local/bin/
-ln -sf ${SCRIPT_DIR}/../llvm-project/build/llvm/bin/jove-i386     /usr/local/bin/
-ln -sf ${SCRIPT_DIR}/../llvm-project/build/llvm/bin/jove-aarch64  /usr/local/bin/
-ln -sf ${SCRIPT_DIR}/../llvm-project/build/llvm/bin/jove-mipsel   /usr/local/bin/
-ln -sf ${SCRIPT_DIR}/../llvm-project/build/llvm/bin/jove-mips64el /usr/local/bin/
+ln -sf ${SCRIPT_DIR}/../../llvm-project/build/llvm/bin/jove-x86_64   /usr/local/bin/
+ln -sf ${SCRIPT_DIR}/../../llvm-project/build/llvm/bin/jove-i386     /usr/local/bin/
+ln -sf ${SCRIPT_DIR}/../../llvm-project/build/llvm/bin/jove-aarch64  /usr/local/bin/
+ln -sf ${SCRIPT_DIR}/../../llvm-project/build/llvm/bin/jove-mipsel   /usr/local/bin/
+ln -sf ${SCRIPT_DIR}/../../llvm-project/build/llvm/bin/jove-mips64el /usr/local/bin/
