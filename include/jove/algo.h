0ea1f2f409b146139b6ef51ad50600cb6e65342c
Aoto Tokizawa <aoto.tokizawa@gmail.com>
2025-11-10 12:28:31 -0800

    algo: work for const

--- include/jove/algo.h
+++ include/jove/algo.h
@@ -7,7 +7,7 @@ template <typename _ExecutionPolicy, typename Iter, typename Pred, typename Proc
 constexpr
 void for_each_if(_ExecutionPolicy &&__exec, Iter first, Iter last, Pred pred, Proc proc) {
   std::for_each(std::forward<_ExecutionPolicy>(__exec), first, last,
-                [pred, proc](auto &elem) {
+                [pred, proc](auto &&elem) {
                   if (pred(elem)) {
                     proc(elem);
                   }
@@ -18,7 +18,7 @@ template <typename Iter, typename Pred, typename Proc>
 constexpr
 void for_each_if(Iter first, Iter last, Pred pred, Proc proc) {
   std::for_each(first, last,
-                [pred, proc](auto &elem) {
+                [pred, proc](auto &&elem) {
                   if (pred(elem)) {
                     proc(elem);
                   }
