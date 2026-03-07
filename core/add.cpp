9696e63dab18be0828f4a15891c50b10759388c3
Aoto Tokizawa <aoto.tokizawa@gmail.com>
2026-03-07 11:00:45 -0800

    add: do not absorb arbitrary exceptions

--- core/add.cpp
+++ core/add.cpp
@@ -44,15 +44,11 @@ void jv_base_t<MT, MinSize>::DoAdd(binary_base_t<MT2, MinSize2> &b,
       return invalid_basic_block_index;
     }
 
-    try {
-      if (Options.IsVeryVerbose())
-        llvm::errs() << llvm::formatv("exploring {0}:{1:x}\n", b.Name.c_str(),
-                                      Entrypoint);
+    if (Options.IsVeryVerbose())
+      llvm::errs() << llvm::formatv("exploring {0}:{1:x}\n", b.Name.c_str(),
+                                    Entrypoint);
 
-      return explorer.explore_basic_block(b, Bin, Entrypoint);
-    } catch (...) {
-      return invalid_basic_block_index;
-    }
+    return explorer.explore_basic_block(b, Bin, Entrypoint);
   };
   auto FunctionAtAddress = [&](uint64_t Entrypoint) -> function_index_t {
     if (!Entrypoint)
