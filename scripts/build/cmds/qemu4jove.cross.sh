6bd118c71b403bf28d3f15506a85795bba5fdad7
Aoto Tokizawa <aoto.tokizawa@gmail.com>
2026-09-17 17:02:09 -0700

    scripts: build: style (comment)

--- scripts/build/cmds/qemu4jove.cross.sh
+++ scripts/build/cmds/qemu4jove.cross.sh
@@ -1,5 +1,9 @@
 #!/bin/false
 
+#
+# this is weird, but unfortunately necessary, since tcgconstants.h changes
+#
+
 for arch in "${all_archs[@]}"; do
   thedir="${hostarch}_carbon_build_${arch}"
   cmds+=("pushd \"$qemu_path\" && mkdir -p $thedir && cd $thedir && retry \"$build_scripts_path/build_qemu.sh -a ${hostarch} -t ${arch} -D -F -c\" && popd")
