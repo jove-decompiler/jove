47b5611a5db1e2da2c5c643a4795d87279098772
Aoto Tokizawa <aoto.tokizawa@gmail.com>
2026-09-21 09:37:55 -0700

    scripts/build: carbon-copy: specify LLVM_DIR

--- scripts/build/carbon-copy/build19.sh
+++ scripts/build/carbon-copy/build19.sh
@@ -5,7 +5,10 @@ set -x
 
 if [ ! -f build.ninja ]; then
 
-CC=clang-19 CXX=clang++-19 cmake -G Ninja -D CMAKE_BUILD_TYPE=Release -S $(pwd)/.. -B $(pwd)
+CC=clang-19 CXX=clang++-19 cmake -G Ninja \
+    -D CMAKE_BUILD_TYPE=Debug \
+    -D LLVM_DIR=/usr/lib/llvm-19/lib/cmake/llvm \
+    -D LLVM_ENABLE_ASSERTIONS=ON -S $(pwd)/.. -B $(pwd)
 
 fi
 
